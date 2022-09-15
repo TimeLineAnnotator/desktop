ad011f9e22656c25341fbd5f813b9a81865ad287
jiraffarij <felipe.martins4@hotmail.com>
2022-09-15 13:37:15 -0300

    Module holds ParentChildRelation class and process_parent_child_relation function, which were previously methods of objects.

---
+++ tilia/timelines/hierarchy/common.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from typing import NamedTuple
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from tilia.timelines.hierarchy.components import Hierarchy
+    from tilia.ui.tkinter.timelines.hierarchy import HierarchyTkUI
+
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class ParentChildRelation(NamedTuple):
+    """Named tuple to facilitate the handling of parent and children attribute of hierarchies."""
+
+    parent: Hierarchy | HierarchyTkUI
+    children: list[Hierarchy | HierarchyTkUI]
+
+
+def process_parent_child_relation(relation: ParentChildRelation):
+    """Changes parent and child atributes of the units
+            that are arguments of the relation as to end with the parent/child
+            structure given."""
+
+    logger.debug(f"Processing parent/child relation '{relation}'")
+
+    parent, children = relation
+
+    logger.debug(f"Clearing children list of {parent}.")
+    parent.children = []
+    for child in children:
+        logger.debug(f"Appending {child} to children list.")
+        parent.children.append(child)
+        logger.debug(f"Setting {parent} as parent of {child}")
+        child.parent = parent
