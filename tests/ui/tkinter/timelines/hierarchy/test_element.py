9cd65f2ce81bba83ccc13fa997725f7317b67f0d
jiraffarij <felipe.martins4@hotmail.com>
2022-09-15 13:24:28 -0300

    Deleted unused test.

--- tests/ui/tkinter/timelines/hierarchy/test_element.py
+++ tests/ui/tkinter/timelines/hierarchy/test_element.py
@@ -5,8 +5,3 @@ import pytest
 
 from tilia.timelines.hierarchy.components import Hierarchy
 from tilia.ui.tkinter.timelines.hierarchy import HierarchyTkUI, HierarchyTimelineTkUI
-
-
-class TestHierarchyTkUI:
-    def test_constructor(self, hrui_params_mock):
-        pass
