ca672374f0b5ab053f9a1aa997bbf5b22ffada03
jiraffarij <felipe.martins4@hotmail.com>
2022-09-15 13:35:56 -0300

    Module that holds the Clipboard class.

---
+++ tilia/clipboard.py
@@ -0,0 +1,18 @@
+"""
+Defines the Clipboard object, that holds the
+data copied by the user.
+"""
+
+from tilia.events import Subscriber, EventName
+
+
+class Clipboard(Subscriber):
+    def __init__(self):
+        super().__init__(subscriptions=[EventName.TIMELINE_COMPONENT_COPIED])
+        self._contents = []
+
+    def on_subscribed_event(
+        self, event_name: EventName, *args: tuple, **kwargs: dict
+    ) -> None:
+        if event_name == EventName.TIMELINE_COMPONENT_COPIED:
+            self._contents = args[0]
