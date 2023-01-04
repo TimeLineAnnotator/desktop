6a0149c944cbddcb7604b913c7a4524d71905f2c
Felipe Martins <felipe.martins4@hotmail.com>
2023-01-04 18:59:09 -0300

    Removed type hint from func
    It was causing NameErrors when testing, for some unknown reason

--- tilia/local_dev_code.py
+++ tilia/local_dev_code.py
@@ -1,7 +1,4 @@
-from tilia.main import TiLiA
-
-
-def func(tilia: TiLiA) -> None:
+def func(tilia) -> None:
     """
     This function will be called right befor the ui mainloop on every startup.
     Use this function to run code for development purposes.
