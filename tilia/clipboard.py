aa49d3e5fd09939595470f59397f05c4a359dc81
jiraffarij <felipe.martins4@hotmail.com>
2023-04-27 22:36:17 -0300

    fix Clipboard._contents typing

--- tilia/clipboard.py
+++ tilia/clipboard.py
@@ -1,7 +1,4 @@
-"""
-Defines the Clipboard object, that holds the
-data copied by the user.
-"""
+from typing import TypedDict, Optional
 
 from tilia import events
 from tilia.events import Event
@@ -9,17 +6,22 @@ from tilia.repr import default_str
 from tilia.timelines.timeline_kinds import TimelineKind
 
 
+class ClipboardContents(TypedDict):
+    components: dict[str, dict]
+    timeline_kind: Optional[TimelineKind]
+
+
 class Clipboard:
     def __init__(self) -> None:
         events.subscribe(
             self, Event.TIMELINE_COMPONENT_COPIED, self.on_timeline_component_copied
         )
-        self._contents = []
+        self._contents: ClipboardContents = {"components": {}, "timeline_kind": None}
 
     def __str__(self):
         return default_str(self)
 
-    def get_contents_for_pasting(self) -> dict[str : dict | TimelineKind]:
+    def get_contents_for_pasting(self) -> ClipboardContents:
         return self._contents
 
     def on_timeline_component_copied(self, contents):
