deec1233fc9e20db8ff757dadf6e60f6f34a4103
jiraffarij <felipe.martins4@hotmail.com>
2023-05-02 10:41:58 -0300

    create a TiliaDialog class for concrete dialogs to inherit from

--- tests/ui/dialogs/test_choose.py
+++ tests/ui/dialogs/test_choose.py
@@ -1,7 +1,7 @@
-from tilia.ui.windows.choose import ChooseWindow
+from tilia.ui.dialogs.choose import ChooseDialog
 
 
-class TestChooseWindow:
+class TestChooseDialog:
 
     options = [
         (1, "first"),
@@ -10,26 +10,26 @@ class TestChooseWindow:
         (4, "fourth"),
     ]
 
-    def test_cancel(self):
-        window = ChooseWindow("", "", [])
+    def test_cancel(self, tkui):
+        window = ChooseDialog(tkui.root, "", "", [])
         window.on_cancel()
         assert window.return_value is False
 
-    def test_destroy(self):
-        window = ChooseWindow("", "", [])
+    def test_destroy(self, tkui):
+        window = ChooseDialog(tkui.root, "", "", [])
         window.destroy()
 
         assert window.return_value is None
 
-    def test_choose_first(self):
-        window = ChooseWindow("", "", self.options)
+    def test_choose_first(self, tkui):
+        window = ChooseDialog(tkui.root, "", "", self.options)
         window.get_selected_index = lambda: 0
         window.on_ok()
 
         assert window.return_value == 1
 
-    def test_choose_last(self):
-        window = ChooseWindow("", "", self.options)
+    def test_choose_last(self, tkui):
+        window = ChooseDialog(tkui.root, "", "", self.options)
         window.get_selected_index = lambda: 3
         window.on_ok()
 
