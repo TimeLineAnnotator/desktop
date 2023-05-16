c8da1b53f499c5a554618e608544d35a6f663e67
jiraffarij <felipe.martins4@hotmail.com>
2023-05-16 09:57:19 -0300

    added parser for creating hierarchies from csv

--- tests/parsers/test_csv.py
+++ tests/parsers/test_csv.py
@@ -193,3 +193,162 @@ def test_hierarchies_by_time_from_csv(hierarchy_tlui):
     assert hierarchies[2].end == 3
     assert hierarchies[2].level == 3
     assert hierarchies[2].ui.label == "third"
+
+
+def test_hierarchies_by_measure_from_csv(beat_tlui, hierarchy_tlui):
+    beat_tl = beat_tlui.timeline
+    hierarchy_tl = hierarchy_tlui.timeline
+    beat_tl.beat_pattern = [1]
+    beat_tlui.create_beat(time=1)
+    beat_tlui.create_beat(time=2)
+    beat_tlui.create_beat(time=3)
+    beat_tlui.create_beat(time=4)
+
+    beat_tl.recalculate_measures()
+
+    os.chdir(Path(Path(__file__).absolute().parents[1]))
+
+    data = "start,end,level,label\n1,2,1,a\n2,3,2,b\n3,4,3,c"
+
+    with patch("builtins.open", mock_open(read_data=data)):
+        hierarchies_by_measure_from_csv(
+            hierarchy_tl,
+            beat_tl,
+            Path(),
+        )
+
+    hierarchies = hierarchy_tl.ordered_hierarchies
+
+    assert hierarchies[0].start == 1
+    assert hierarchies[0].end == 2
+    assert hierarchies[0].level == 1
+    assert hierarchies[0].ui.label == "a"
+
+    assert hierarchies[1].start == 2
+    assert hierarchies[1].end == 3
+    assert hierarchies[1].level == 2
+    assert hierarchies[1].ui.label == "b"
+
+    assert hierarchies[2].start == 3
+    assert hierarchies[2].end == 4
+    assert hierarchies[2].level == 3
+    assert hierarchies[2].ui.label == "c"
+
+
+def test_hierarchies_by_measure_from_csv_outputs_error_if_bad_start_value(
+    hierarchy_tlui, beat_tlui
+):
+    data = "start,end,level\nnonsense, 1, 1"
+    with patch("builtins.open", mock_open(read_data=data)):
+        errors = hierarchies_by_measure_from_csv(
+            hierarchy_tlui.timeline, beat_tlui.timeline, Path()
+        )
+
+    assert "nonsense" in errors[0]
+
+
+def test_hierarchies_by_measure_from_csv_outputs_error_if_bad_end_value(
+    hierarchy_tlui, beat_tlui
+):
+    data = "start,end,level\n1, nonsense, 1"
+    with patch("builtins.open", mock_open(read_data=data)):
+        errors = hierarchies_by_measure_from_csv(
+            hierarchy_tlui.timeline, beat_tlui.timeline, Path()
+        )
+
+    assert "nonsense" in errors[0]
+
+
+def test_hierarchies_by_measure_from_csv_outputs_error_if_bad_level_value(
+    hierarchy_tlui, beat_tlui
+):
+    data = "start,end,level\n1, 1, nonsense"
+    with patch("builtins.open", mock_open(read_data=data)):
+        errors = hierarchies_by_measure_from_csv(
+            hierarchy_tlui.timeline, beat_tlui.timeline, Path()
+        )
+
+    assert "nonsense" in errors[0]
+
+
+def test_hierarchies_by_measure_from_csv_outputs_error_if_bad_start_fraction_value(
+    hierarchy_tlui, beat_tlui
+):
+    beat_tl = beat_tlui.timeline
+    hierarchy_tl = hierarchy_tlui.timeline
+    beat_tl.beat_pattern = [1]
+    beat_tlui.create_beat(time=1)
+    beat_tlui.create_beat(time=2)
+
+    data = "start,start_fraction,end,level\n1,nonsense, 2, 1"
+    with patch("builtins.open", mock_open(read_data=data)):
+        errors = hierarchies_by_measure_from_csv(
+            hierarchy_tlui.timeline, beat_tlui.timeline, Path()
+        )
+
+    assert "nonsense" in errors[0]
+
+    assert hierarchy_tl.ordered_hierarchies[0].start == 1
+
+
+def test_hierarchies_by_measure_from_csv_outputs_error_if_bad_end_fraction_value(
+    hierarchy_tlui, beat_tlui
+):
+    beat_tl = beat_tlui.timeline
+    hierarchy_tl = hierarchy_tlui.timeline
+    beat_tl.beat_pattern = [1]
+    beat_tlui.create_beat(time=1)
+    beat_tlui.create_beat(time=2)
+
+    data = "start,end,end_fraction,level\n1, 2, nonsense, 1"
+    with patch("builtins.open", mock_open(read_data=data)):
+        errors = hierarchies_by_measure_from_csv(
+            hierarchy_tlui.timeline, beat_tlui.timeline, Path()
+        )
+
+    assert "nonsense" in errors[0]
+
+    assert hierarchy_tl.ordered_hierarchies[0].start == 1
+
+
+def test_hierarchies_by_measure_from_csv_outputs_error_if_no_measure_found(
+    hierarchy_tlui, beat_tlui
+):
+    beat_tl = beat_tlui.timeline
+    beat_tl.beat_pattern = [1]
+    beat_tlui.create_beat(time=1)
+    beat_tlui.create_beat(time=2)
+
+    data = "start,end,level\n3, 4, 1"
+    with patch("builtins.open", mock_open(read_data=data)):
+        errors = hierarchies_by_measure_from_csv(
+            hierarchy_tlui.timeline, beat_tlui.timeline, Path()
+        )
+
+    assert "No measure with number" in errors[0]
+
+    data = "start,end,level\n1, 5, 1"
+    with patch("builtins.open", mock_open(read_data=data)):
+        errors = hierarchies_by_measure_from_csv(
+            hierarchy_tlui.timeline, beat_tlui.timeline, Path()
+        )
+
+    assert "No measure with number" in errors[0]
+
+
+def test_hierarchies_by_measure_from_csv_bad_optional_attrs_values(
+    hierarchy_tlui, beat_tlui
+):
+    beat_tl = beat_tlui.timeline
+    beat_tl.beat_pattern = [1]
+    beat_tlui.create_beat(time=1)
+    beat_tlui.create_beat(time=2)
+
+    data = "start,end,level,pre_start,post_end,\n1,2,1,nonsense1,nonsense2"
+    with patch("builtins.open", mock_open(read_data=data)):
+        errors = hierarchies_by_measure_from_csv(
+            hierarchy_tlui.timeline, beat_tlui.timeline, Path()
+        )
+
+    assert "nonsense1" in errors[0]
+    assert "nonsense2" in errors[1]
--- tilia/parsers/csv.py
+++ tilia/parsers/csv.py
@@ -244,3 +244,168 @@ def hierarchies_by_time_from_csv(
                 errors.append(f"{start=}, {end=}, {level=} | {str(exc)}")
 
         return errors
+
+
+def hierarchies_by_measure_from_csv(
+    hierarchy_tl: HierarchyTimeline,
+    beat_tl: BeatTimeline,
+    path: Path,
+    file_kwargs: Optional[dict[str, Any]] = None,
+    reader_kwargs: Optional[dict[str, Any]] = None,
+) -> list[str]:
+    """
+    Create hierarchies in a timeline from a csv file with 1-based measure indices.
+    Assumes the first row of the file will contain headers.
+    Header names should match hierarchy propertiesAll properties
+    but 'measure' are optional.
+    Returns an array with descriptions of any CreateComponentErrors
+    raised during hierarchy creation.
+
+    Note: The measure column should have measure indices, not measure numbers.
+    That means that repeated measure numbers should not be taken into account.
+    """
+
+    errors = []
+
+    with TiliaCSVReader(path, file_kwargs, reader_kwargs) as reader:
+        header = next(reader)
+
+        required_params = [
+            (
+                "start",
+                float,
+            ),
+            (
+                "end",
+                float,
+            ),
+            ("level", int),
+        ]
+
+        optional_params = [
+            (
+                "start_fraction",
+                float,
+            ),
+            (
+                "end_fraction",
+                float,
+            ),
+            (
+                "pre_start",
+                float,
+            ),
+            (
+                "pre_start_fraction",
+                float,
+            ),
+            (
+                "post_end",
+                float,
+            ),
+            (
+                "post_end_fraction",
+                float,
+            ),
+            (
+                "label",
+                str,
+            ),
+            (
+                "color",
+                str,
+            ),
+            (
+                "comments",
+                str,
+            ),
+            (
+                "formal_type",
+                str,
+            ),
+            (
+                "formal_function",
+                str,
+            ),
+        ]
+
+        params_to_indices = get_params_indices([p[0] for p in required_params], header)
+        params_to_indices.update(
+            get_params_indices([p[0] for p in optional_params], header)
+        )
+
+        for attr, _ in required_params:
+            if attr not in params_to_indices:
+                raise ValueError(f"Column '{attr}' not found on first row of csv file.")
+
+        for row in reader:
+            required_values = {}
+            try:
+                for attr, parser in required_params:
+                    index = params_to_indices[attr]
+                    value = row[index]
+                    required_values[attr] = parser(value)
+            except ValueError:
+                errors.append(f"{value=} | {value} is not a valid {attr}")
+                continue
+
+            # get and validate fraction
+            fractions = {"start": 0, "end": 0}
+
+            for ext in fractions:
+                attr = ext + "_fraction"
+                if attr in params_to_indices:
+                    fraction_value = row[params_to_indices[attr]]
+                    try:
+                        fractions[ext] = float(fraction_value)
+                    except ValueError:
+                        errors.append(
+                            f"start={required_values['start']}, end={required_values['end']} | {fraction_value} is not a fraction value. Defaulting to 0."
+                        )
+                        fractions[ext] = 0
+
+            times = {"start": 0, "end": 0}
+
+            # get and validate times
+            for ext in times:
+                times[ext] = beat_tl.get_time_by_measure(
+                    required_values[ext], fractions[ext]
+                )
+                if not times[ext]:
+                    value = required_values[ext]
+                    errors.append(f"'{ext}={value} | No measure with number {value}")
+                    continue
+
+            # get remaining params
+            kwargs = {}
+            reamining_params = [
+                p
+                for p in optional_params
+                if p[0] not in ["start_fraction", "end_fraction"]
+            ]
+            for param, parser in reamining_params:
+                if param in params_to_indices:
+                    index = params_to_indices[param]
+                    try:
+                        kwargs[param] = parser(row[index])
+                    except ValueError:
+                        errors.append(
+                            f"'start'={required_values['start']}, end={required_values['end']} | '{row[index]}' is not a valid {param} value."
+                        )
+
+            # create hierarchies
+            for start, end in zip(times["start"], times["end"]):
+                try:
+                    hierarchy_tl.create_timeline_component(
+                        ComponentKind.HIERARCHY,
+                        start,
+                        end,
+                        required_values["level"],
+                        start_fraction=fractions["start"],
+                        end_fraction=fractions["end"],
+                        **kwargs,
+                    )
+                except CreateComponentError as exc:
+                    errors.append(f"{start=}, {end=} | {str(exc)}")
+
+        return errors
