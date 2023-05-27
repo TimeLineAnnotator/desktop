e8ac746ce0cea11ee5049b837dc5c40d701ba07e
jiraffarij <felipe.martins4@hotmail.com>
2023-05-27 00:27:19 -0300

    add minimal implementation of HierarchyTimelineUI and HierarchyUI so HierarchyTimline can be created

--- tilia/ui/cli/timelines/hierarchy/timeline.py
+++ tilia/ui/cli/timelines/hierarchy/timeline.py
@@ -1,9 +1,11 @@
 from tilia import settings
 from tilia.timelines.timeline_kinds import TimelineKind
 from tilia.ui.cli.timelines.base import TimelineUI
+from tilia.ui.cli.timelines.hierarchy import HierarchyUI
 
 
 class HierarchyTimelineUI(TimelineUI):
     TIMELINE_KIND = TimelineKind.HIERARCHY_TIMELINE
+    ELEMENT_CLASS = HierarchyUI
     height = settings.get("hierarchy_timeline", "default_height")
     is_visible = True
--- tilia/ui/protocol/timelines/timeline.py
+++ tilia/ui/protocol/timelines/timeline.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
-from typing import Protocol, TYPE_CHECKING
+from typing import Protocol, TYPE_CHECKING, Optional
 
 from tilia.timelines.timeline_kinds import TimelineKind
 
 if TYPE_CHECKING:
+    from tilia.ui.cli.timelines.base import TimelineUIElement
     from tilia.timelines.common import TimelineComponent
     from tilia.timelines.component_kinds import ComponentKind
 
 
 class TimelineUI(Protocol):
     TIMELINE_KIND: TimelineKind
+    ELEMENT_CLASS: Optional[TimelineUIElement]
     name: str
     is_visible: bool
     height: int
@@ -18,4 +20,7 @@ class TimelineUI(Protocol):
     def get_ui_for_component(
         self, component_kind: ComponentKind, component: TimelineComponent, **kwargs
     ):
+        ...
+
+    def create_element(self, component: TimelineComponent, **kwargs):
         ...
