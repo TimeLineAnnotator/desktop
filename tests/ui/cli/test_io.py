503e7d7056246cf09f57227ba759a18a474f7b09
jiraffarij <felipe.martins4@hotmail.com>
2023-06-08 12:18:16 -0300

    rename cli.output to cli.io and add some functions to get inut from user

---
+++ tests/ui/cli/test_io.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+
+import pytest
+
+from tilia.ui.cli.io import ask_yes_or_no
+
+
+class TestInput:
+    @pytest.mark.parametrize("user_input,expected", [
+        ("y", True),
+        ("Y", True),
+        ("yes", True),
+        ("YES", True),
+        ("Yes", True),
+        ("n", False),
+        ("N", False),
+        ("no", False),
+        ('NO', False),
+        ('anything', False),
+        ('', False)
+    ])
+    def test_ask_yes_or_no(self, user_input, expected):
+        with patch('builtins.input', return_value=user_input):
+            assert ask_yes_or_no("Some prompt") == expected
--- tilia/ui/cli/io.py
+++ tilia/ui/cli/io.py
@@ -19,3 +19,24 @@ def tabulate(headers: list[str], data: list[tuple[str, ...]]) -> None:
     table.field_names = headers
     table.add_rows(data)
     print(str(table))
+
+
+def ask_for_string(prompt: str) -> str:
+    """
+    Prompts the user for a string
+    """
+    return input(prompt)
+
+
+def ask_for_directory(prompt: str) -> str:
+    """
+    Prompts the user for a directory
+    """
+    return input(prompt)
+
+
+def ask_yes_or_no(prompt: str) -> bool:
+    """
+    Prompts the user for a yes or no answer
+    """
+    return input(prompt + ' (y)es/(n)o: ').lower() in ['y', 'yes']
