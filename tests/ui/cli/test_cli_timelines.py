ba07210d3cca3517f130b580a299d3316ad90f67
jiraffarij <felipe.martins4@hotmail.com>
2023-06-08 10:04:34 -0300

    add timeline remove command

--- tests/ui/cli/test_cli_timelines.py
+++ tests/ui/cli/test_cli_timelines.py
@@ -1,60 +1,156 @@
-from tilia.timelines.timeline_kinds import TimelineKind
-
+import pytest
 from unittest.mock import patch
+
+from tilia.timelines.timeline_kinds import TimelineKind
 
 
 class TestTimelineAdd:
     def test_add_hierarchy_timeline(self, tls, cli):
-        cli.run(['timeline', 'add', 'hrc', '--name', 'test'])
+        cli.run(["timeline", "add", "hrc", "--name", "test"])
 
         tl = tls.get_timelines()[0]
         assert tl.KIND == TimelineKind.HIERARCHY_TIMELINE
-        assert tl.name == 'test'
+        assert tl.name == "test"
 
     def test_add_marker_timeline(self, tls, cli):
-        cli.run(['timeline', 'add', 'mrk', '--name', 'test'])
+        cli.run(["timeline", "add", "mrk", "--name", "test"])
 
         tl = tls.get_timelines()[0]
         assert tl.KIND == TimelineKind.MARKER_TIMELINE
-        assert tl.name == 'test'
+        assert tl.name == "test"
 
 
 class TestTimelineList:
     def test_list_timelines_no_timelines(self, tls, cli):
-        with patch('builtins.print') as mock_print:
-            cli.run(['timeline', 'list'])
+        with patch("builtins.print") as mock_print:
+            cli.run(["timeline", "list"])
 
             printed = mock_print.call_args[0][0]
 
-            assert 'name' in printed
-            assert 'ord' in printed
-            assert 'kind' in printed
-            assert '1' not in printed
+            assert "name" in printed
+            assert "ord" in printed
+            assert "kind" in printed
+            assert "1" not in printed
 
     def test_list_timelines_single_timeline(self, tls, cli):
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name='test1')
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test1")
 
         tl = tls.get_timelines()[0]
-        with patch('builtins.print') as mock_print:
-            cli.run(['timeline', 'list'])
+        with patch("builtins.print") as mock_print:
+            cli.run(["timeline", "list"])
 
             printed = mock_print.call_args[0][0]
 
-            assert 'test1' in printed
-            assert 'Hierarchy' in printed
+            assert "test1" in printed
+            assert "Hierarchy" in printed
             assert tl.id in printed
 
     def test_list_timelines_multiple_timelines(self, tls, cli):
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name='test1')
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name='test2')
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name='test3')
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test1")
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test2")
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test3")
 
         tl = tls.get_timelines()[0]
-        with patch('builtins.print') as mock_print:
-            cli.run(['timeline', 'list'])
+        with patch("builtins.print") as mock_print:
+            cli.run(["timeline", "list"])
 
             printed = mock_print.call_args[0][0]
 
-            assert 'test1' in printed
-            assert 'test2' in printed
-            assert 'test3' in printed
+            assert "test1" in printed
+            assert "test2" in printed
+            assert "test3" in printed
+
+
+class TestTimelineRemove:
+    def test_remove_type_not_provide(self, tls, cli):
+        cli.run(["timeline", "remove"])
+
+        assert type(cli.exception) == SystemExit
+
+    def test_remove_by_name_one_timeline(self, tls, cli):
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test1")
+
+        cli.run(["timeline", "remove", "name", "test1"])
+
+        assert len(tls) == 0
+
+    def test_remove_by_name_multiple_timelines(self, tls, cli):
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test1")
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test2")
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test3")
+
+        cli.run(["timeline", "remove", "name", "test1"])
+
+        assert len(tls) == 2
+
+        cli.run(["timeline", "remove", "name", "test3"])
+
+        assert len(tls) == 1
+
+    def test_remove_by_name_not_found(self, tls, cli):
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test1")
+
+        with patch("builtins.print") as mock_print:
+            cli.run(["timeline", "remove", "name", "othername"])
+
+            printed = mock_print.call_args[0][0]
+            assert "No timeline found" in printed
+            assert "othername" in printed
+        assert type(cli.exception) == ValueError
+
+    def test_remove_by_name_when_no_timelines(self, tls, cli):
+        with patch("builtins.print") as mock_print:
+            cli.run(["timeline", "remove", "name", "test"])
+
+            printed = mock_print.call_args[0][0]
+            assert "No timeline found" in printed
+            assert "test" in printed
+
+    def test_remove_by_name_name_not_provide(self, tls, cli):
+        cli.run(["timeline", "remove", "name"])
+
+        assert type(cli.exception) == SystemExit
+
+    def test_remove_by_ordinal_one_timeline(self, tls, cli):
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test1")
+
+        cli.run(["timeline", "remove", "ordinal", "1"])
+
+        assert len(tls) == 0
+
+    def test_remove_by_ordinal_multiple_timelines(self, tls, cli):
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE)
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE)
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE)
+
+        cli.run(["timeline", "remove", "ordinal", "1"])
+
+        assert len(tls) == 2
+
+        cli.run(["timeline", "remove", "ordinal", "1"])
+
+        assert len(tls) == 1
+
+    def test_remove_by_ordinal_not_found(self, tls, cli):
+        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE)
+
+        with patch("builtins.print") as mock_print:
+            cli.run(["timeline", "remove", "ordinal", "3"])
+
+            printed = mock_print.call_args[0][0]
+            assert "No timeline found" in printed
+            assert "3" in printed
+        assert type(cli.exception) == ValueError
+
+    def test_remove_by_ordinal_when_no_timelines(self, tls, cli):
+        with patch("builtins.print") as mock_print:
+            cli.run(["timeline", "remove", "ordinal", "1"])
+
+            printed = mock_print.call_args[0][0]
+            assert "No timeline found" in printed
+            assert "1" in printed
+
+    def test_remove_by_ordinal_ordinal_not_provide(self, tls, cli):
+        cli.run(["timeline", "remove", "ordinal"])
+
+        assert type(cli.exception) == SystemExit
--- tilia/ui/cli/timelines.py
+++ tilia/ui/cli/timelines.py
@@ -6,11 +6,11 @@ from tilia.ui.cli.output import tabulate
 
 
 def setup_parser(subparsers: Subparsers):
-    tl = subparsers.add_parser("timeline")
+    tl = subparsers.add_parser("timeline", exit_on_error=False)
     tl_subparser = tl.add_subparsers(dest="timeline_command")
 
     # 'add' subparser
-    add_subp = tl_subparser.add_parser("add")
+    add_subp = tl_subparser.add_parser("add", exit_on_error=False)
     add_subp.add_argument(
         "kind", choices=["hierarchy", "hrc", "marker", "mrk", "beat", "bea"]
     )
@@ -18,15 +18,24 @@ def setup_parser(subparsers: Subparsers):
     add_subp.set_defaults(func=add)
 
     # 'list' subparser
-    list_subp = tl_subparser.add_parser("list")
+    list_subp = tl_subparser.add_parser("list", exit_on_error=False)
     list_subp.set_defaults(func=list)
 
     # 'remove' subparser
-    remove_subp = tl_subparser.add_parser("remove")
-    remove_subp.add_mutually_exclusive_group(required=True)
-    remove_subp.add_argument("--name", "-n")
-    remove_subp.add_argument("--id")
-    # remove.set_defaults(func=remove_timeline)
+    remove_subp = tl_subparser.add_parser("remove", exit_on_error=False)
+    remove_subcommands = remove_subp.add_subparsers(dest="type", required=True)
+
+    # 'remove by name' subcommand
+    remove_by_name_subc = remove_subcommands.add_parser("name", exit_on_error=False)
+    remove_by_name_subc.add_argument("name")
+    remove_by_name_subc.set_defaults(func=remove_by_name)
+
+    # 'remove by ordinal' subcommand
+    remove_by_ordinal_subc = remove_subcommands.add_parser(
+        "ordinal", exit_on_error=False
+    )
+    remove_by_ordinal_subc.add_argument("ordinal", type=int)
+    remove_by_ordinal_subc.set_defaults(func=remove_by_ordinal)
 
 
 def add(namespace):
@@ -59,6 +68,28 @@ def list(_):
         for tl in timelines
     ]
     tabulate(headers, data)
+
+
+def remove_by_name(namespace):
+    tl = get(Get.TIMELINE_BY_ATTR, "name", namespace.name)
+
+    if not tl:
+        raise ValueError(f"No timeline found with name={namespace.name}")
+
+    print(f"Removing timeline {tl=}")
+
+    post(Post.REQUEST_TIMELINE_DELETE, tl.id)
+
+
+def remove_by_ordinal(namespace):
+    tl = get(Get.TIMELINE_BY_ATTR, "ordinal", namespace.ordinal)
+
+    if not tl:
+        raise ValueError(f"No timeline found with ordinal={namespace.ordinal}")
+
+    print(f"Removing timeline {tl=}")
+
+    post(Post.REQUEST_TIMELINE_DELETE, tl.id)
 
 
 def get_timeline_by_name(name: str) -> Timeline:
