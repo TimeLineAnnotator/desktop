a05d53987639701f38a9ca576a2ea30eb8d4771b
jiraffarij <felipe.martins4@hotmail.com>
2023-06-14 18:28:00 -0300

    use argparse.Namespace instead of EmptyObject

--- tests/ui/cli/test_save.py
+++ tests/ui/cli/test_save.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import tempfile
 
@@ -9,27 +10,34 @@ from unittest.mock import patch
 from tests.conftest import EmptyObject
 from tests.mock import PatchPost
 from tilia.requests import Post
-from tilia.ui.cli.save import ensure_tla_extension, ask_overwrite_save_path, \
-    validate_save_path, save
+from tilia.ui.cli.save import (
+    ensure_tla_extension,
+    ask_overwrite_save_path,
+    validate_save_path,
+    save,
+)
 
 
-@pytest.mark.parametrize("path, expected", [
-    ("file.txt", "file.txt.tla"),
-    ("file", "file.tla"),
-    ("file.tla", "file.tla"),
-])
+@pytest.mark.parametrize(
+    "path, expected",
+    [
+        ("file.txt", "file.txt.tla"),
+        ("file", "file.tla"),
+        ("file.tla", "file.tla"),
+    ],
+)
 def test_ensure_tla_extension(path, expected):
     result = ensure_tla_extension(Path(path))
     assert str(result) == expected
 
 
 def test_ask_overwrite_save_path_yes(monkeypatch):
-    with patch('builtins.input', return_value='yes'):
+    with patch("builtins.input", return_value="yes"):
         assert ask_overwrite_save_path(Path("file.tla")) is True
 
 
 def test_ask_overwrite_save_path_no(monkeypatch):
-    with patch('builtins.input', return_value='no'):
+    with patch("builtins.input", return_value="no"):
         assert ask_overwrite_save_path(Path("file.tla")) is False
 
 
@@ -54,9 +62,8 @@ def test_validate_save_path_parent_is_file():
 def test_save_no_overwrite_existing_file(monkeypatch, tmp_path):
     path = tmp_path / "file.tla"
     path.write_text("content")
-    monkeypatch.setattr('sys.stdin', StringIO('no\n'))
-    namespace = EmptyObject()
-    namespace.path = str(path)
+    monkeypatch.setattr("sys.stdin", StringIO("no\n"))
+    namespace = argparse.Namespace(path=str(path))
     save(namespace)
     assert path.read_text() == "content"
 
@@ -64,9 +71,8 @@ def test_save_no_overwrite_existing_file(monkeypatch, tmp_path):
 def test_save_overwrite_existing_file(monkeypatch, tmp_path):
     path = tmp_path / "file.tla"
     path.write_text("content")
-    monkeypatch.setattr('sys.stdin', StringIO('yes\n'))
-    namespace = EmptyObject()
-    namespace.path = str(path)
+    monkeypatch.setattr("sys.stdin", StringIO("yes\n"))
+    namespace = argparse.Namespace(path=str(path))
     with PatchPost("tilia.ui.cli.save", Post.REQUEST_SAVE_TO_PATH) as post_mock:
         save(namespace)
         post_mock.assert_called_once_with(Post.REQUEST_SAVE_TO_PATH, path)
@@ -74,8 +80,7 @@ def test_save_overwrite_existing_file(monkeypatch, tmp_path):
 
 def test_save_non_existing_file(monkeypatch, tmp_path):
     path = tmp_path / "file.tla"
-    namespace = EmptyObject()
-    namespace.path = str(path)
+    namespace = argparse.Namespace(path=str(path))
     with PatchPost("tilia.ui.cli.save", Post.REQUEST_SAVE_TO_PATH) as post_mock:
         save(namespace)
         post_mock.assert_called_once_with(Post.REQUEST_SAVE_TO_PATH, path)
@@ -83,15 +88,13 @@ def test_save_non_existing_file(monkeypatch, tmp_path):
 
 def test_save_invalid_save_path(monkeypatch, tmp_path):
     path = tmp_path / "invalid" / "file.tla"
-    namespace = EmptyObject()
-    namespace.path = str(path)
+    namespace = argparse.Namespace(path=str(path))
     with pytest.raises(ValueError):
         save(namespace)
 
 
 def test_save_valid_save(tilia, tmp_path):
     path = tmp_path / "test.tla"
-    namespace = EmptyObject()
-    namespace.path = str(path)
+    namespace = argparse.Namespace(path=str(path))
     save(namespace)
     assert os.listdir(tmp_path) == [path.name]
