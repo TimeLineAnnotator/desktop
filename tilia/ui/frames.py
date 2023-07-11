6f810be63ce2440305b463a9b6496b2564be7fb2
jiraffarij <felipe.martins4@hotmail.com>
2023-07-11 17:31:34 -0300

    go back to differentiated color for ScrollableFrame

--- tilia/ui/frames.py
+++ tilia/ui/frames.py
@@ -35,7 +35,7 @@ class ScrollableFrame(tk.Frame):
 
     def __init__(self, parent) -> None:
         super().__init__(parent)
-        self.canvas = tk.Canvas(self, borderwidth=0, highlightthickness=0, bg=TimelineCanvas.DEFAULT_BG)
+        self.canvas = tk.Canvas(self, borderwidth=0, highlightthickness=0, bg='light gray')
         self.frame = tk.Frame(self.canvas)
         self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
         self.canvas.configure(yscrollcommand=self.vsb.set)
