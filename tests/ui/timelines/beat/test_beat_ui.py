a0897897a76ec84590b4c1d9e833387f92f6c682
jiraffarij <felipe.martins4@hotmail.com>
2024-02-08 16:16:57 -0300

    refactor to test behavior instead of implementation

--- tests/ui/timelines/beat/test_beat_ui.py
+++ tests/ui/timelines/beat/test_beat_ui.py
@@ -1,7 +1,9 @@
 from unittest.mock import Mock, patch
 
-from tests.mock import PatchPost
-from tilia.requests import Post, get, Get
+from PyQt6.QtCore import Qt
+
+from tilia.requests import get, Get, Post, post
+from tilia.ui.coords import get_x_by_time
 
 
 class TestRightClick:
@@ -15,16 +17,42 @@ class TestRightClick:
         exec_mock.assert_called_once()
 
 
+def click_beat_ui(beat_ui, button='left', modifier=None, double=False):
+    request = {
+        'left': Post.TIMELINE_VIEW_LEFT_CLICK,
+        'right': Post.TIMELINE_VIEW_RIGHT_CLICK,
+    }[button]
+
+    modifier = {
+        None: Qt.KeyboardModifier.NoModifier
+    }[modifier]
+
+    post(
+        request,
+        beat_ui.timeline_ui.view,
+        get_x_by_time(beat_ui.time),
+        beat_ui.height / 2,
+        beat_ui.body,
+        modifier,
+        double=double,
+    )
+
+
+def move_mouse_to(tlui, x, y):
+    post(Post.TIMELINE_VIEW_LEFT_BUTTON_DRAG, x, y)
+
+
 class TestDoubleClick:
-    def test_triggers_seek(self, beat_tlui):
-        beat_tlui.create_beat(10)
-        beat_tlui[0].on_double_left_click(None)
+    def test_triggers_seek(self, beat_tlui, qtbot):
+        b, bui = beat_tlui.create_beat(10)
+        click_beat_ui(bui, double=True)
         assert get(Get.MEDIA_CURRENT_TIME) == 10
 
     def test_does_not_trigger_drag(self, beat_tlui):
-        beat_tlui.create_beat(0)
-        mock = Mock()
-        beat_tlui[0].setup_drag = mock
-        beat_tlui[0].on_double_left_click(None)
+        _, bui = beat_tlui.create_beat(0)
+        click_beat_ui(bui, double=True)
+        move_mouse_to(beat_tlui, int(get_x_by_time(50)), int(bui.height / 2))
+        assert bui.get_data('time') == 0
 
-        mock.assert_not_called()
+
+
