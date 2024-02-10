2c3943bc265ce53c1c8141c59c70d4f54912c4d0
jiraffarij <felipe.martins4@hotmail.com>
2024-02-10 12:26:15 -0300

    fix typing of timeline dummy

--- tests/timelines/hierarchy/test_hierarchy_component.py
+++ tests/timelines/hierarchy/test_hierarchy_component.py
@@ -1,8 +1,11 @@
 from tilia.timelines.hierarchy.components import Hierarchy
+from tilia.timelines.hierarchy.timeline import HierarchyTimeline
 
 
-class DummyHierarchyTL:
-    pass
+class DummyHierarchyTL(HierarchyTimeline):
+    # noinspection PyMissingConstructor
+    def __init__(self):
+        pass
 
 
 class TestHierarchyComponent:
@@ -28,43 +31,43 @@ class TestHierarchyComponent:
             assert getattr(hrc, attr) == params[attr]
 
     def test_create_no_pre_start_or_end(self):
-        hrc = Hierarchy(DummyHierarchyTL(), None, 0, 1, 1)
+        hrc = Hierarchy(DummyHierarchyTL(), 0, 0, 1, 1)
 
         assert hrc.pre_start == 0
         assert hrc.post_end == 1
 
     def test_set_start_changes_pre_start_previously_equal(self):
-        hrc = Hierarchy(DummyHierarchyTL(), None, 1, 2, 1)
+        hrc = Hierarchy(DummyHierarchyTL(), 0, 1, 2, 1)
 
         hrc.start = 1.5
         assert hrc.pre_start == 1.5
 
     def test_set_start_changes_pre_start_not_previously_equal_1(self):
-        hrc = Hierarchy(DummyHierarchyTL(), None, 1, 2, 1, pre_start=0.5)
+        hrc = Hierarchy(DummyHierarchyTL(), 0, 1, 2, 1, pre_start=0.5)
 
         hrc.start = 0.8
         assert hrc.pre_start == 0.5
 
     def test_set_start_changes_pre_start_not_previously_equal_2(self):
-        hrc = Hierarchy(DummyHierarchyTL(), None, 1, 2, 1, pre_start=0.5)
+        hrc = Hierarchy(DummyHierarchyTL(), 0, 1, 2, 1, pre_start=0.5)
 
         hrc.start = 0.4
         assert hrc.pre_start == 0.4
 
     def test_set_end_changes_post_end_previously_equal(self):
-        hrc = Hierarchy(DummyHierarchyTL(), None, 1, 2, 1)
+        hrc = Hierarchy(DummyHierarchyTL(), 0, 1, 2, 1)
 
         hrc.end = 1.5
         assert hrc.post_end == 1.5
 
     def test_set_end_changes_post_end_not_previously_equal_1(self):
-        hrc = Hierarchy(DummyHierarchyTL(), None, 1, 2, 1, post_end=2.5)
+        hrc = Hierarchy(DummyHierarchyTL(), 0, 1, 2, 1, post_end=2.5)
 
         hrc.end = 2.3
         assert hrc.post_end == 2.5
 
     def test_set_end_changes_post_end_not_previously_equal_2(self):
-        hrc = Hierarchy(DummyHierarchyTL(), None, 1, 2, 1, post_end=2.5)
+        hrc = Hierarchy(DummyHierarchyTL(), 0, 1, 2, 1, post_end=2.5)
 
         hrc.end = 2.8
         assert hrc.post_end == 2.8
