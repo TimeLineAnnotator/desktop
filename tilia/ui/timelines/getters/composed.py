e5060e127a3a569e87e8e9fa31c4e07df0aa10fa
jiraffarij <felipe.martins4@hotmail.com>
2024-02-12 08:05:24 -0300

    delete unused modules and folders

--- tilia/ui/timelines/getters/composed.py
+++
@@ -1,7 +0,0 @@
-from . import basic
-
-
-def last_selected_by_kind(kinds):
-    return basic.compose(
-        [(basic.by_kinds, (kinds,), {}), (basic.last_selected, (), {})]
-    )
--- tilia/ui/windows/exceptions.py
+++
@@ -1,9 +0,0 @@
-from .kinds import WindowKind
-
-
-class UniqueWindowDuplicate(Exception):
-    def __init__(self, kind: WindowKind):
-        self.kind = kind
-
-    def __str__(self):
-        return f"Can't instance a second window of kind '{self.kind.value}'"
