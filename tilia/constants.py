84200ccbd7b4920e13c8fa41cf38e4b68ec93d74
jiraffarij <felipe.martins4@hotmail.com>
2024-02-13 17:00:14 -0300

    change version number to 0.3

--- tilia/constants.py
+++ tilia/constants.py
@@ -3,5 +3,5 @@ from pathlib import Path
 APP_NAME = "TiLiA"
 APP_ICON_PATH = Path("ui", "img", "main_icon.png")
 FILE_EXTENSION = "tla"
-VERSION = "0.1.1"
+VERSION = "0.3"
 YOUTUBE_URL_REGEX = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
