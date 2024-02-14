059e5fab26c7bf04f25f60a41e16a9352eb64ca2
jiraffarij <felipe.martins4@hotmail.com>
2024-02-14 17:08:02 -0300

    fix error when user closed file dialogs instead of choosing a file

--- tilia/ui/dialogs/file.py
+++ tilia/ui/dialogs/file.py
@@ -15,13 +15,17 @@ def ask_should_save_changes():
     return ask_yes_no_or_cancel("Save changes", "Save changes to current file?")
 
 
+def _get_return_from_file_dialog(dialog: QFileDialog):
+    return dialog.exec(), dialog.selectedFiles()[0] if dialog.selectedFiles() else None
+
+
 def ask_for_tilia_file_to_open():
     dialog = QFileDialog()
     dialog.setWindowTitle(f"Open {APP_NAME} files")
     dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
     dialog.setFilter(QDir.Filter.Files)
     dialog.setNameFilters([APP_FILE_FILTER, "All files (*)"])
-    return dialog.exec(), dialog.selectedFiles()[0]
+    return _get_return_from_file_dialog(dialog)
 
 
 def ask_for_file_to_open(title: str, name_filters: Sequence[str]):
@@ -30,7 +34,7 @@ def ask_for_file_to_open(title: str, name_filters: Sequence[str]):
     dialog.setFilter(QDir.Filter.Files)
     dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
     dialog.setNameFilters(name_filters)
-    return dialog.exec(), dialog.selectedFiles()[0]
+    return _get_return_from_file_dialog(dialog)
 
 
 def ask_for_path_to_save_tilia_file(initial_filename: str) -> tuple[str, str]:
@@ -78,4 +82,4 @@ def ask_for_media_file():
         ]
     )
 
-    return dialog.exec(), dialog.selectedFiles()[0]
+    return _get_return_from_file_dialog(dialog)
