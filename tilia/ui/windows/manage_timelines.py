f7554bc3856adfe662b239ef474004878a6ba654
Anne Foo <45888544+azfoo@users.noreply.github.com>
2024-05-20 17:10:05 +0100

    Added missing titles to windows

--- tilia/ui/windows/manage_timelines.py
+++ tilia/ui/windows/manage_timelines.py
@@ -30,6 +30,7 @@ from tilia.ui.timelines.base.timeline import TimelineUI
 class ManageTimelines(QDialog):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Manage Timelines")
         self._setup_widgets()
         self._setup_checkbox()
         self._setup_requests()
