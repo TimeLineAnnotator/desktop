0b87aa32e8283fb6484dfa95ce34fc3a62904cd8
azfoo <45888544+azfoo@users.noreply.github.com>
2024-06-05 16:42:20 +0100

    Fix Crash Handler Sizing
    Put Exception message in a scrollable area so window size remains manageable.

--- tilia/ui/dialogs/crash.py
+++ tilia/ui/dialogs/crash.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QFrame
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QFrame, QScrollArea
 
 
 class CrashDialog(QDialog):
@@ -15,16 +15,25 @@ class CrashDialog(QDialog):
 
         sorry_label = QLabel('TiLiA has crashed with the following error:')
         sorry_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        exc_info_text_edit = QLabel(self.exception_info)
+
+        exc_info_text_edit = QLabel(f"<pre style='white-space: pre-wrap;'>{self.exception_info}</pre>")
         exc_info_text_edit.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
-        exc_info_text_edit.setFrameShadow(QFrame.Shadow.Sunken)
-        exc_info_text_edit.setFrameShape(QFrame.Shape.Box)
+        exc_info_text_edit.setWordWrap(True)
+        exc_info_text_edit.setContentsMargins(5, 5, 5, 5)
+
+        exc_info_scroll = QScrollArea()
+        exc_info_scroll.setWidget(exc_info_text_edit)
+        exc_info_scroll.setAutoFillBackground(False)
+        exc_info_scroll.setSizeAdjustPolicy(QScrollArea.SizeAdjustPolicy.AdjustToContents)
+        exc_info_scroll.setWidgetResizable(True)
+        exc_info_scroll.setFrameShadow(QFrame.Shadow.Sunken)
+        exc_info_scroll.setFrameShape(QFrame.Shape.Panel)
+
         unsaved_changes_label = QLabel('Unsaved work will be lost, but TiLiA autosaves your work periodically.\nTo access saved files, restart TiLiA and go to File > Autosaves.')
         unsaved_changes_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         layout.addWidget(sorry_label)
-        layout.addWidget(exc_info_text_edit)
+        layout.addWidget(exc_info_scroll)
         layout.addWidget(unsaved_changes_label)
 
-        # layout.setContentsMargins(10, 20, 20, 10)
         layout.setSpacing(5)
