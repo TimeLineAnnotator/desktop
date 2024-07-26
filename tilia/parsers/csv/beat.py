46746f0d0b4b0455c6fdb4d2ed0a56054d2a4cf3
azfoo <45888544+azfoo@users.noreply.github.com>
2024-07-26 03:04:38 +0100

    update csv beat import
    measure_number column:
    - input number in column if measure number change is required. subsequent bars will count up from given number. will also cause ui to force display specified measure's number.
    
    is_first_in_measure column:
    - input `True` if given time is first beat of measure.

--- tilia/parsers/csv/beat.py
+++ tilia/parsers/csv/beat.py
@@ -18,39 +18,116 @@ def beats_from_csv(
 ) -> list[str]:
     """
     Create beat in a timeline from times extracted from a csv file.
-    Assumes the first row of the file will contain a header named 'time'.
-    Returns an array with descriptions of any errors during the process.
+    Assumes the first row of the file will contain headers.
+    At least 'time' should be present.
+    'measure_number' and 'is_first_in_measure' are optional.
+    Returns an array with descriptions of any CreateComponentErrors
+    raised during beat creation.
+
+    Please ensure 'time' column is sorted (ascending) before input.
     """
 
     errors = []
+    params = ["is_first_in_measure", "measure_number", "time"]
+    optional_params = ["is_first_in_measure", "measure_number"]
+    parsers = [str, int, float]
+    params_to_indices = []
 
     with TiliaCSVReader(path, file_kwargs, reader_kwargs) as reader:
-        params_to_indices = get_params_indices(["time"], next(reader))
+        params_to_indices = get_params_indices(params, next(reader))
 
         if "time" not in params_to_indices:
             display_column_not_found_error("time")
             return errors
 
+        if any(param in params_to_indices for param in optional_params):
+            if "is_first_in_measure" in params_to_indices:
+                is_reading_beats = True
+                timeline.beat_pattern = []
+            else:
+                is_reading_beats = False
+                beats_per_measure = timeline.beat_pattern
+                current_bpm_index = 0
+
+            current_beat = 0
+            current_measure = 1
+            current_time = 0.0
+            beats_in_measure = []
+            measure_numbers = []
+            measures_to_force_display = []
+
+            def check_params() -> bool:
+                for param, parser in zip(params, parsers):
+                    if param in params_to_indices:
+                        try:
+                            index = params_to_indices[param]
+                            if row[index] != "" or param not in optional_params:
+                                parser(row[index])
+                                return True
+                        except ValueError:
+                            errors.append(
+                                f"{row}'{row[index]}' is not a valid {param.replace('_', ' ')}"
+                            )
+                            return False
+
+            for row in reader:
+                if not row:
+                    continue
+
+                if not check_params():
+                    return errors
+
+                if float(row[params_to_indices.get("time")]) < current_time:
+                    errors.append(
+                        "Time is not sorted in ascending order. Please sort before importing."
+                    )
+                    return errors
+                else:
+                    current_time = float(row[params_to_indices.get("time")])
+
+                if (
+                    params_to_indices.get("is_first_in_measure")
+                    and row[params_to_indices["is_first_in_measure"]].lower() == "true"
+                    and current_beat != 0
+                ) or (
+                    not is_reading_beats
+                    and current_beat >= beats_per_measure[current_bpm_index]
+                ):
+                    beats_in_measure.append(current_beat)
+                    measure_numbers.append(current_measure)
+                    current_beat = 1
+                    current_measure += 1
+
+                    if not is_reading_beats:
+                        current_bpm_index = (current_bpm_index + 1) % len(
+                            beats_per_measure
+                        )
+
+                else:
+                    current_beat += 1
+
+                if (
+                    params_to_indices.get("measure_number")
+                    and row[params_to_indices["measure_number"]] != ""
+                ):
+                    current_measure = int(row[params_to_indices["measure_number"]])
+                    measures_to_force_display.append(len(measure_numbers))
+
+            beats_in_measure.append(current_beat)
+            measure_numbers.append(current_measure)
+            timeline.set_data("beat_pattern", beats_in_measure)
+            timeline.set_data("measure_numbers", measure_numbers)
+            timeline.set_data("measures_to_force_display", measures_to_force_display)
+
+    with TiliaCSVReader(path, file_kwargs, reader_kwargs) as reader:
+        next(reader)
         for row in reader:
             if not row:
                 continue
-            # validate time
-            index = params_to_indices["time"]
-            time_value = row[index]
-            try:
-                int(time_value)
-            except ValueError:
-                errors.append(f"{time_value=} | {time_value} is not a valid time")
-                continue
 
-            params = ["time"]
-            parsers = [float]
             constructor_kwargs = {}
-
-            for param, parser in zip(params, parsers):
-                if param in params_to_indices:
-                    index = params_to_indices[param]
-                    constructor_kwargs[param] = parser(row[index])
+            index = params_to_indices["time"]
+            constructor_kwargs["time"] = float(row[index])
 
             component, fail_reason = timeline.create_timeline_component(
                 ComponentKind.BEAT, **constructor_kwargs
