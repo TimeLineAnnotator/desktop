1a4eec83d8b750507f7afa7ca04caf3dc8a0a114
azfoo <45888544+azfoo@users.noreply.github.com>
2024-07-31 17:58:15 +0100

    refactor: create variable with all media types

--- tilia/media/constants.py
+++ tilia/media/constants.py
@@ -6,3 +6,11 @@ CONVERTIBLE_AUDIO_FORMATS = [
     "flac",
 ]  # ffmpeg can probably convert more formats
 SUPPORTED_VIDEO_FORMATS = ["mp4", "mkv", "m4a"]
+ALL_SUPPORTED_MEDIA_FORMATS = {
+    media_format
+    for media_format in {
+        *SUPPORTED_AUDIO_FORMATS,
+        *CONVERTIBLE_AUDIO_FORMATS,
+        *SUPPORTED_VIDEO_FORMATS,
+    }
+}
