6d929d7760df76b1b18cfcfba652da5f0599df95
jiraffarij <felipe.martins4@hotmail.com>
2024-09-12 19:04:53 -0300

    Fix 'save' CLI command

--- tests/ui/cli/test_save.py
+++ tests/ui/cli/test_save.py
@@ -1,20 +1,11 @@
-import argparse
 import os
-import tempfile
 
 import pytest
 from pathlib import Path
 from io import StringIO
-from unittest.mock import patch
 
-from tests.mock import PatchPost
-from tilia.requests import Post
-from tilia.ui.cli.save import (
-    ensure_tla_extension,
-    ask_overwrite_save_path,
-    validate_save_path,
-    save,
-)
+from tests.ui.cli.common import cli_run
+from tilia.ui.cli.save import ensure_tla_extension
 
 
 @pytest.mark.parametrize(
@@ -30,70 +21,48 @@ def test_ensure_tla_extension(path, expected):
     assert str(result) == expected
 
 
-def test_ask_overwrite_save_path_yes(monkeypatch):
-    with patch("builtins.input", return_value="yes"):
-        assert ask_overwrite_save_path(Path("file.tla")) is True
+def test_save(tmp_path):
+    path = tmp_path / "test.tla"
+    cli_run(f'save "{str(path.resolve())}"')
+    assert os.listdir(tmp_path) == [path.name]
 
 
-def test_ask_overwrite_save_path_no(monkeypatch):
-    with patch("builtins.input", return_value="no"):
-        assert ask_overwrite_save_path(Path("file.tla")) is False
+def test_save_parent_is_file_fails(tmp_path, tilia_errors):
+    parent = tmp_path / "parent"
+    parent.touch()
+    save_path = tmp_path / 'parent' / "file.tla"
+    cli_run(f'save "{str(save_path.resolve())}"')
+    tilia_errors.assert_error()
+    assert not save_path.exists()
 
 
-def test_validate_save_path_valid(tmp_path):
-    path = tmp_path / "file.tla"
-    validate_save_path(path)
+def test_save_parent_doesnt_exist_fails(tmp_path, tilia_errors):
+    save_path = tmp_path / 'parent' / "file.tla"
+    cli_run(f'save "{str(save_path.resolve())}"')
+    tilia_errors.assert_error()
+    assert not save_path.exists()
 
 
-def test_validate_save_path_parent_dir_doesnt_exist(tmp_path):
-    path = tmp_path / "invalid" / "file.tla"
-    with pytest.raises(ValueError):
-        validate_save_path(path)
-
-
-def test_validate_save_path_parent_is_file():
-    with tempfile.NamedTemporaryFile() as tmp_file:
-        path = Path(tmp_file.name, "file.tla")
-        with pytest.raises(ValueError):
-            validate_save_path(path)
-
-
-def test_save_no_overwrite_existing_file(monkeypatch, tmp_path):
-    path = tmp_path / "file.tla"
-    path.write_text("content")
-    monkeypatch.setattr("sys.stdin", StringIO("no\n"))
-    namespace = argparse.Namespace(path=str(path))
-    save(namespace)
-    assert path.read_text() == "content"
-
-
-def test_save_overwrite_existing_file(monkeypatch, tmp_path):
+def test_save_overwrite_yes(monkeypatch, tmp_path):
     path = tmp_path / "file.tla"
     path.write_text("content")
     monkeypatch.setattr("sys.stdin", StringIO("yes\n"))
-    namespace = argparse.Namespace(path=str(path))
-    with PatchPost("tilia.ui.cli.save", Post.REQUEST_SAVE_TO_PATH) as post_mock:
-        save(namespace)
-        post_mock.assert_called_once_with(Post.REQUEST_SAVE_TO_PATH, path)
+    cli_run(f'save "{str(path.resolve())}"')
+    assert path.read_text() != "content"
 
 
-def test_save_non_existing_file(monkeypatch, tmp_path):
+def test_save_overwrite_no(monkeypatch, tmp_path):
     path = tmp_path / "file.tla"
-    namespace = argparse.Namespace(path=str(path))
-    with PatchPost("tilia.ui.cli.save", Post.REQUEST_SAVE_TO_PATH) as post_mock:
-        save(namespace)
-        post_mock.assert_called_once_with(Post.REQUEST_SAVE_TO_PATH, path)
+    path.write_text("content")
+    monkeypatch.setattr("sys.stdin", StringIO("no\n"))
+    cli_run(f'save "{str(path.resolve())}"')
+    assert path.read_text() == "content"
 
 
-def test_save_invalid_save_path(monkeypatch, tmp_path):
+def test_save_invalid_save_path(tmp_path, tilia_errors):
     path = tmp_path / "invalid" / "file.tla"
-    namespace = argparse.Namespace(path=str(path))
-    with pytest.raises(ValueError):
-        save(namespace)
+    cli_run(f'save "{str(path.resolve())}"')
+    tilia_errors.assert_error()
 
 
-def test_save_valid_save(tilia, tmp_path):
-    path = tmp_path / "test.tla"
-    namespace = argparse.Namespace(path=str(path))
-    save(namespace)
-    assert os.listdir(tmp_path) == [path.name]
+
