660552b8a6c46e395ebab4e3d1eb551bc47d54c8
jiraffarij <felipe.martins4@hotmail.com>
2024-09-12 15:30:34 -0300

    Enable CLI tests again
    Some tests are still failing due to actual bugs in the CLI. This is what we get for turning off the tests for so long :)

--- tests/ui/cli/test_load_media.py
+++ tests/ui/cli/test_load_media.py
@@ -1,9 +1,10 @@
 from tests.mock import PatchPost
+from tests.ui.cli.common import cli_run
 from tilia.requests import Post
 
 
-def test_load_media(cli):
+def test_load_media():
     with PatchPost("tilia.ui.cli.load_media", Post.APP_MEDIA_LOAD) as mock_post:
-        cli.run(["load-media", "test"])
+        cli_run("load-media test")
 
     mock_post.assert_called_with(Post.APP_MEDIA_LOAD, "test")
--- tests/ui/cli/timelines/test_cli_timelines.py
+++ tests/ui/cli/timelines/test_cli_timelines.py
@@ -1,12 +1,16 @@
 from unittest.mock import patch
 
+from tests.mock import Serve
+from tests.ui.cli.common import cli_run
+from tilia.requests import Get
 from tilia.timelines.timeline_kinds import TimelineKind
+from tilia.ui.actions import TiliaAction
 
 
 class TestTimelineList:
-    def test_list_timelines_no_timelines(self, tls, cli):
+    def test_list_timelines_no_timelines(self):
         with patch("builtins.print") as mock_print:
-            cli.run(["timeline", "list"])
+            cli_run('timeline list')
 
             printed = mock_print.call_args[0][0]
 
@@ -15,27 +19,25 @@ class TestTimelineList:
             assert "kind" in printed
             assert "1" not in printed
 
-    def test_list_timelines_single_timeline(self, tls, cli):
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test1")
+    def test_list_timelines_single_timeline(self, hierarchy_tl, actions):
+        hierarchy_tl.set_data('name', 'test1')
 
-        tl = tls.get_timelines()[0]
         with patch("builtins.print") as mock_print:
-            cli.run(["timeline", "list"])
+            cli_run('timeline list')
 
             printed = mock_print.call_args[0][0]
 
             assert "test1" in printed
             assert "Hierarchy" in printed
-            assert str(tl.ordinal) in printed
+            assert str(hierarchy_tl.ordinal) in printed
 
-    def test_list_timelines_multiple_timelines(self, tls, cli):
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test1")
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test2")
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test3")
+    def test_list_timelines_multiple_timelines(self, tls, actions):
+        for name in ["test1", "test2", "test3"]:
+            with Serve(Get.FROM_USER_STRING, (name, True)):
+                actions.trigger(TiliaAction.TIMELINES_ADD_HIERARCHY_TIMELINE)
 
-        _ = tls.get_timelines()[0]
         with patch("builtins.print") as mock_print:
-            cli.run(["timeline", "list"])
+            cli_run('timelines list')
 
             printed = mock_print.call_args[0][0]
 
@@ -45,95 +47,92 @@ class TestTimelineList:
 
 
 class TestTimelineRemove:
-    def test_remove_type_not_provide(self, tls, cli):
-        cli.run(["timeline", "remove"])
+    def test_type_not_provided(self, hierarchy_tl, tls):
+        cli_run('timeline remove')
 
-        assert type(cli.exception) == SystemExit
+        assert not tls.is_empty
 
-    def test_remove_by_name_one_timeline(self, tls, cli):
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test1")
+    def test_by_name_one_timeline(self, tls, actions):
+        with Serve(Get.FROM_USER_STRING, ("test", True)):
+            actions.trigger(TiliaAction.TIMELINES_ADD_HIERARCHY_TIMELINE)
 
-        cli.run(["timeline", "remove", "name", "test1"])
+        cli_run('timeline remove name test')
 
-        assert len(tls) == 0
+        assert tls.is_empty
 
-    def test_remove_by_name_multiple_timelines(self, tls, cli):
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test1")
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test2")
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test3")
+    def test_remove_by_name_multiple_timelines(self, actions, tls):
+        for name in ["test1", "test2", "test3"]:
+            with Serve(Get.FROM_USER_STRING, (name, True)):
+                actions.trigger(TiliaAction.TIMELINES_ADD_HIERARCHY_TIMELINE)
 
-        cli.run(["timeline", "remove", "name", "test1"])
+        cli_run('timeline remove name test1')
 
         assert len(tls) == 2
 
-        cli.run(["timeline", "remove", "name", "test3"])
+        cli_run(["timeline", "remove", "name", "test2"])
 
         assert len(tls) == 1
 
-    def test_remove_by_name_not_found(self, tls, cli):
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test1")
+    def test_remove_by_name_not_found(self, tls, actions):
+        with Serve(Get.FROM_USER_STRING, ("test", True)):
+            actions.trigger(TiliaAction.TIMELINES_ADD_HIERARCHY_TIMELINE)
 
         with patch("builtins.print") as mock_print:
-            cli.run(["timeline", "remove", "name", "othername"])
+            cli_run('timeline remove name othername')
 
             printed = mock_print.call_args[0][0]
             assert "No timeline found" in printed
             assert "othername" in printed
-        assert type(cli.exception) == ValueError
 
-    def test_remove_by_name_when_no_timelines(self, tls, cli):
+    def test_remove_by_name_when_no_timelines(self):
         with patch("builtins.print") as mock_print:
-            cli.run(["timeline", "remove", "name", "test"])
+            cli_run('timeline remove name test')
 
             printed = mock_print.call_args[0][0]
             assert "No timeline found" in printed
             assert "test" in printed
 
-    def test_remove_by_name_name_not_provide(self, tls, cli):
-        cli.run(["timeline", "remove", "name"])
+    def test_remove_by_name_name_not_provide(self, tls, hierarchy_tl):
+        cli_run('timeline remove name')
 
-        assert type(cli.exception) == SystemExit
+        assert not tls.is_empty
 
-    def test_remove_by_ordinal_one_timeline(self, tls, cli):
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE, name="test1")
-
-        cli.run(["timeline", "remove", "ordinal", "1"])
+    def test_remove_by_ordinal_one_timeline(self, tls, hierarchy_tl):
+        cli_run('timeline remove ordinal 1')
 
         assert len(tls) == 0
 
-    def test_remove_by_ordinal_multiple_timelines(self, tls, cli):
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE)
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE)
-        tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE)
+    def test_remove_by_ordinal_multiple_timelines(self, actions, tls):
+        with Serve(Get.FROM_USER_STRING, ("", True)):
+            actions.trigger(TiliaAction.TIMELINES_ADD_HIERARCHY_TIMELINE)
 
-        cli.run(["timeline", "remove", "ordinal", "1"])
+        cli_run('timeline remove ordinal 1')
 
         assert len(tls) == 2
 
-        cli.run(["timeline", "remove", "ordinal", "1"])
+        cli_run(["timeline", "remove", "ordinal", "2"])
 
         assert len(tls) == 1
 
-    def test_remove_by_ordinal_not_found(self, tls, cli):
+    def test_remove_by_ordinal_not_found(self, tls):
         tls.create_timeline(TimelineKind.HIERARCHY_TIMELINE)
 
-        with patch("builtins.print") as mock_print:
-            cli.run(["timeline", "remove", "ordinal", "3"])
+        with patch("bultins.print") as mock_print:
+            cli_run('timeline remove ordinal 3')
 
             printed = mock_print.call_args[0][0]
             assert "No timeline found" in printed
             assert "3" in printed
-        assert type(cli.exception) == ValueError
 
-    def test_remove_by_ordinal_when_no_timelines(self, tls, cli):
+    def test_remove_by_ordinal_when_no_timelines(self, tls):
         with patch("builtins.print") as mock_print:
-            cli.run(["timeline", "remove", "ordinal", "1"])
+            cli_run('timeline remove ordinal 1')
 
             printed = mock_print.call_args[0][0]
             assert "No timeline found" in printed
             assert "1" in printed
 
-    def test_remove_by_ordinal_ordinal_not_provide(self, tls, cli):
-        cli.run(["timeline", "remove", "ordinal"])
+    def test_remove_by_ordinal_ordinal_not_provided(self, tls, hierarchy_tl):
+        cli_run('timeline remove ordinal')
 
-        assert type(cli.exception) == SystemExit
+        assert not tls.is_empty
