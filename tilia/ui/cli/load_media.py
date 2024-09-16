aa8fc5f383a80d11bc4bc82063415c6ddd5fb625
jiraffarij <felipe.martins4@hotmail.com>
2024-09-16 08:56:55 -0300

    Fix 'load-media' CLI command

--- tilia/ui/cli/load_media.py
+++ tilia/ui/cli/load_media.py
@@ -15,6 +15,13 @@ def setup_parser(subparsers):
         help="Path to media.",
     )
 
+    parser.add_argument(
+        "-s", "--scale-timelines",
+        type=str,
+        choices=["yes", "no", "prompt"],
+        default='prompt',
+        help="Automatically scale the media timeline.",
+    )
     parser.set_defaults(func=load_media)
 
 
@@ -24,11 +31,12 @@ def load_media(namespace):
         tilia.errors.display(tilia.errors.MEDIA_NOT_FOUND, path)
         return
 
-    post(Post.APP_MEDIA_LOAD, str(path.resolve()).replace("\\", "/"))
+    post(Post.APP_MEDIA_LOAD, str(path.resolve()).replace("\\", "/"), scale_timelines=namespace.scale_timelines)
 
-    time.sleep(0.1)  # conservative estimate for QMediaPlayer to laod the file
+    time.sleep(0.1)  # conservative estimate for QMediaPlayer to load the file
     duration = get(Get.MEDIA_DURATION)
     if duration:
         io.output(f"Media loaded, duration is {duration}.")
     else:
         io.output("No media duration available, loading may have failed. You can set a duration manually with 'metadata set-media-length'")
+
