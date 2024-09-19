79f4dbf0ece87e2c13e5401fa8c762c486b8b6dd
jiraffarij <felipe.martins4@hotmail.com>
2024-09-19 16:10:47 -0300

    Bump version

--- tilia/constants.py
+++ tilia/constants.py
@@ -3,7 +3,7 @@ from pathlib import Path
 APP_NAME = "TiLiA"
 APP_ICON_PATH = Path("ui", "img", "main_icon.png")
 FILE_EXTENSION = "tla"
-VERSION = "0.4.1"
+VERSION = "0.4.3"
 GITHUB_URL = "https://github.com/FelipeDefensor/TiLiA"
 WEBSITE_URL = "https://tilia-ad98d.web.app/"
 YOUTUBE_URL_REGEX = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
