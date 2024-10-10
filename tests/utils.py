75b5749fbe46b005adb9ea6c5e87bb9bb92b56b6
jiraffarij <felipe.martins4@hotmail.com>
2024-10-10 16:58:44 -0300

    Refactor tests for opening tla files
    The next commits will move the open functionality out of FileManager and
    into App and a standalone function. This refactor prepares the tests
    file-structure for that.

--- tests/utils.py
+++ tests/utils.py
@@ -30,3 +30,16 @@ def get_blank_file_data():
         "app_name": "TiLiA",
         "version": "0.1.1"
     }
+
+
+def get_dummy_timeline_data(id: int = 1) -> dict[str, dict]:
+    return {
+        str(id): {
+            "height": 220,
+            "is_visible": True,
+            "ordinal": 1,
+            "name": "test",
+            "kind": "HIERARCHY_TIMELINE",
+            "components": {},
+        }
+    }
