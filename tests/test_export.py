d27906c8d06205b769724657a5324e0a1f781dda
jiraffarij <felipe.martins4@hotmail.com>
2024-10-20 18:47:15 -0300

    Fix `test_export.py`

--- tests/test_export.py
+++ tests/test_export.py
@@ -12,10 +12,10 @@ from tilia.timelines.marker.components import Marker
 from tilia.ui.actions import TiliaAction
 
 
-def test_export_timelines(tilia, marker_tlui, harmony_tlui, actions, tilia_state, tmp_path):
+def test_export_timelines(tilia, marker_tlui, harmony_tlui, user_actions, tilia_state, tmp_path):
     for i in range(5):
         tilia_state.current_time = i
-        actions.trigger(TiliaAction.MARKER_ADD)
+        user_actions.trigger(TiliaAction.MARKER_ADD)
 
     harmony_tlui.create_harmony(0)
     harmony_tlui.create_mode(0)
@@ -33,15 +33,15 @@ def test_export_timelines(tilia, marker_tlui, harmony_tlui, actions, tilia_state
 
     # check marker timeline components
     assert data['timelines'][0]['component_kinds'] == [Marker.KIND.name]
-    assert data['timelines'][0]['component_attributes'][ComponentKind.MARKER.name] == Marker.SERIALIZABLE_BY_VALUE
+    assert data['timelines'][0]['component_attributes'][ComponentKind.MARKER.name] == Marker.get_export_attributes()
     assert len(data['timelines'][0]['components'][ComponentKind.MARKER.name]) == 5
 
     # check harmony timeline components
     assert Harmony.KIND.name in data['timelines'][1]['component_kinds']
     assert Mode.KIND.name in data['timelines'][1]['component_kinds']
     assert data['timelines'][1]['component_attributes'] == {
-        'HARMONY': Harmony.SERIALIZABLE_BY_VALUE,
-        'MODE': Mode.SERIALIZABLE_BY_VALUE
+        'HARMONY': Harmony.get_export_attributes(),
+        'MODE': Mode.get_export_attributes()
     }
     assert len(data['timelines'][1]['components'][ComponentKind.HARMONY.name]) == 1
     assert len(data['timelines'][1]['components'][ComponentKind.MODE.name]) == 1
@@ -101,8 +101,10 @@ def test_export_attributes_are_present(tilia, hierarchy_tl, tmp_path):
         'level',
         'label',
         'comments',
-        'start_metric_pos',
-        'end_metric_pos',
+        'start_measure',
+        'start_beat',
+        'end_measure',
+        'end_beat',
         'length',
         'length_in_measures'
     ]
