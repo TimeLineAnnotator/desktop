65749a0dfe81437d0c43b81b9c68c743e28532cf
jiraffarij <felipe.martins4@hotmail.com>
2024-10-25 16:40:06 -0300

    WIP change NoteUI shape to improve readability
    I hope we can do better than this

--- tilia/ui/timelines/score/element/note/body.py
+++ tilia/ui/timelines/score/element/note/body.py
@@ -1,15 +1,15 @@
 from __future__ import annotations
 
 from PyQt6.QtCore import Qt, QRectF, QPointF
-from PyQt6.QtGui import QColor, QPen
-from PyQt6.QtWidgets import QGraphicsRectItem
+from PyQt6.QtGui import QColor, QPen, QPolygonF
+from PyQt6.QtWidgets import QGraphicsRectItem, QGraphicsPolygonItem
 
 from tilia.ui.color import get_tinted_color, get_untinted_color
 from tilia.ui.consts import TINT_FACTOR_ON_SELECTION
 from tilia.ui.timelines.cursors import CursorMixIn
 
 
-class NoteBody(CursorMixIn, QGraphicsRectItem):
+class NoteBody(CursorMixIn, QGraphicsPolygonItem):
     X_OFFSET = 0
     Y_OFFSET = 1
 
@@ -18,40 +18,52 @@ class NoteBody(CursorMixIn, QGraphicsRectItem):
     ):
         super().__init__(cursor_shape=Qt.CursorShape.PointingHandCursor)
         self.set_position(start_x, end_x, top_y, note_height)
-        self.set_pen_style_no_pen()
+        self.set_pen()
         self.set_fill(color)
 
     def set_fill(self, color: str):
         self.setBrush(QColor(color))
 
-    def set_pen_style_solid(self):
-        pen = QPen(QColor("black"))
+    def set_pen(self):
+        pen = QPen(QColor("#777777"))
+        pen.setWidth(0)
         pen.setStyle(Qt.PenStyle.SolidLine)
         self.setPen(pen)
 
-    def set_pen_style_no_pen(self):
-        pen = QPen(QColor("black"))
-        pen.setStyle(Qt.PenStyle.NoPen)
-        self.setPen(pen)
-
     def set_position(self, start_x: float, end_x: float, top_y: float, note_height: float):
-        self.setRect(self.get_rect(start_x, end_x, top_y, note_height))
+        self.setPolygon(self.get_polygon(start_x, end_x, top_y, note_height))
 
     def on_select(self):
-        self.set_pen_style_solid()
         self.setBrush(
             QColor(get_tinted_color(self.brush().color(), TINT_FACTOR_ON_SELECTION))
         )
 
     def on_deselect(self):
-        self.set_pen_style_no_pen()
         self.setBrush(
             QColor(get_untinted_color(self.brush().color(), TINT_FACTOR_ON_SELECTION))
         )
 
-    def get_rect(self, start_x: float, end_x: float, top_y: float, note_height: float) -> QRectF:
-        x0 = start_x + self.X_OFFSET
-        y0 = top_y + self.Y_OFFSET / 2
-        x1 = end_x - self.X_OFFSET
-        y1 = top_y + note_height - self.Y_OFFSET / 2
-        return QRectF(QPointF(x0, y0), QPointF(x1, y1))
+    def get_polygon(self, start_x: float, end_x: float, top_y: float, note_height: float) -> QPolygonF:
+        indent = 5
+        mid_y = top_y + note_height / 2
+        bottom_y = top_y + note_height
+        x_left_outer = start_x
+        x_left_inner = start_x + indent
+        x_right_inner = end_x - indent
+        x_right_outer = end_x
+        return QPolygonF([
+            QPointF(x_left_outer, mid_y),
+            QPointF(x_left_inner, top_y),
+            QPointF(x_right_inner, top_y),
+            QPointF(x_right_outer, mid_y),
+            QPointF(x_right_inner, bottom_y),
+            QPointF(x_left_inner, bottom_y),
+            QPointF(x_left_outer, mid_y),
+            ]
+        )
+
+        # code for rectangle
+        # x0 = top_y + self.Y_OFFSET / 2
+        # x1 = end_x - self.X_OFFSET
+        # y1 = top_y + note_height - self.Y_OFFSET / 2
+        # return QPolygonF(QPointF(x0, y0), QPointF(x1, y1))
