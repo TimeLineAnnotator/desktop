6979c6c5cf542051a85c95531a9e3527182be6f8
azfoo <45888544+azfoo@users.noreply.github.com>
2024-11-07 16:31:16 +0000

    refactor: remove unused imports

--- tests/ui/cli/timelines/test_cli_timelines_import.py
+++ tests/ui/cli/timelines/test_cli_timelines_import.py
@@ -5,7 +5,6 @@ from tests.mock import Serve
 from tilia.exceptions import WrongTimelineForImport
 from tilia.requests import Get
 from tilia.timelines.timeline_kinds import TimelineKind
-from tilia.ui.actions import TiliaAction
 from tilia.ui.cli.timelines.imp import (
     get_timelines_for_import,
     validate_timelines_for_import,
@@ -22,9 +21,7 @@ def tmp_csv(tmp_path, data):
 
 
 class TestImportTimeline:
-    def test_markers_by_measure(
-        self, cli, marker_tl, beat_tl, tmp_path, tilia_state, user_actions
-    ):
+    def test_markers_by_measure(self, cli, marker_tl, beat_tl, tmp_path):
         beat_tl.beat_pattern = [1]
         for i in range(5):
             beat_tl.create_beat(i)
@@ -49,9 +46,7 @@ class TestImportTimeline:
         for i in range(5):
             assert marker_tl[i].get_data("time") == i + 1
 
-    def test_hierarchies_by_measure(
-        self, cli, hierarchy_tl, beat_tl, tmp_path, tilia_state, user_actions
-    ):
+    def test_hierarchies_by_measure(self, cli, hierarchy_tl, beat_tl, tmp_path):
         beat_tl.beat_pattern = [1]
         for i in range(6):
             beat_tl.create_beat(i)
@@ -67,9 +62,7 @@ class TestImportTimeline:
             assert hierarchy_tl[i].get_data("start") == i
             assert hierarchy_tl[i].get_data("end") == i + 1
 
-    def test_hierarchies_by_time(
-        self, cli, hierarchy_tl, tmp_path, tilia_state, user_actions
-    ):
+    def test_hierarchies_by_time(self, cli, hierarchy_tl, tmp_path):
         data = "start,end,level\n1,2,1\n2,3,1\n3,4,1\n4,5,1\n5,6,1"
         csv_path = tmp_csv(tmp_path, data)
 
@@ -80,7 +73,7 @@ class TestImportTimeline:
             assert hierarchy_tl[i].get_data("start") == i + 1
             assert hierarchy_tl[i].get_data("end") == i + 2
 
-    def test_beats(self, cli, beat_tl, tmp_path, tilia_state, user_actions):
+    def test_beats(self, cli, beat_tl, tmp_path):
         data = "time\n1\n2\n3\n4\n5"
         csv_path = tmp_csv(tmp_path, data)
 
