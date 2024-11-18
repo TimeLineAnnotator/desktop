1d8434439a18ca4a45e6af6ba8caaf53a25ec668
Felipe Martins <felipe.martins4@hotmail.com>
2024-11-18 11:20:55 -0300

    WIP: use QRectF for note bodies

--- tilia/ui/timelines/score/element/note/body.py
+++ tilia/ui/timelines/score/element/note/body.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-from PyQt6.QtCore import Qt, QPointF
-from PyQt6.QtGui import QColor, QPen, QPolygonF
-from PyQt6.QtWidgets import QGraphicsPolygonItem
+from PyQt6.QtCore import Qt, QPointF, QRectF
+from PyQt6.QtGui import QColor, QPen
+from PyQt6.QtWidgets import QGraphicsRectItem
 
 from tilia.timelines.score.components import Note
 from tilia.ui.color import get_tinted_color, get_untinted_color
@@ -10,7 +10,7 @@ from tilia.ui.consts import TINT_FACTOR_ON_SELECTION
 from tilia.ui.timelines.cursors import CursorMixIn
 
 
-class NoteBody(CursorMixIn, QGraphicsPolygonItem):
+class NoteBody(CursorMixIn, QGraphicsRectItem):
     X_OFFSET = 0.5
     Y_OFFSET = 1
 
@@ -39,7 +39,7 @@ class NoteBody(CursorMixIn, QGraphicsPolygonItem):
         self.setPen(pen)
 
     def set_position(self, start_x: float, end_x: float, top_y: float, note_height: float):
-        self.setPolygon(self.get_polygon(start_x, end_x, top_y, note_height))
+        self.setRect(self.get_rect(start_x, end_x, top_y, note_height))
 
     def on_select(self):
         self.setBrush(
@@ -55,15 +55,12 @@ class NoteBody(CursorMixIn, QGraphicsPolygonItem):
         self.set_no_pen()
         self.setZValue(1)
 
-    def get_polygon(self, start_x: float, end_x: float, top_y: float, note_height: float) -> QPolygonF:
+    def get_rect(self, start_x: float, end_x: float, top_y: float, note_height: float) -> QRectF:
         x0 = start_x + (-1 if self.tie_type == Note.TieType.STOP else self.X_OFFSET)
         x1 = end_x - (-1 if self.tie_type == Note.TieType.START else self.X_OFFSET)
         y0 = top_y + self.Y_OFFSET / 2
         y1 = top_y + note_height - self.Y_OFFSET / 2
-        return QPolygonF(
-            [
+        return QRectF(
                 QPointF(x0, y0),
-                QPointF(x0, y1),
                 QPointF(x1, y1),
-                QPointF(x1, y0)
-            ])
+            )
