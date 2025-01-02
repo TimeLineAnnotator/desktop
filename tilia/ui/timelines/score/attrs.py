47fdf9eff33ca035db0ceb9eefe81241d63bac65
azfoo <45888544+azfoo@users.noreply.github.com>
2025-01-02 22:15:31 +0000

    feat: more readable note inspector

--- tilia/ui/timelines/score/attrs.py
+++ tilia/ui/timelines/score/attrs.py
@@ -1,12 +1,13 @@
 from tilia.ui.windows.inspect import InspectRowKind
 
 INSPECTOR_FIELDS = [
-    ('Start', InspectRowKind.LABEL, None),
-    ('End', InspectRowKind.LABEL, None),
-    ('Step', InspectRowKind.LABEL, None),
-    ('Accidental', InspectRowKind.LABEL, None),
-    ('Octave', InspectRowKind.LABEL, None),
-    ('Comments', InspectRowKind.MULTI_LINE_EDIT, None),
+    ("Start", InspectRowKind.LABEL, None),
+    ("End", InspectRowKind.LABEL, None),
+    ("Start / end (metric)", InspectRowKind.LABEL, None),
+    ("Note", InspectRowKind.LABEL, None),
+    ("Comments", InspectRowKind.MULTI_LINE_EDIT, None),
 ]
 
-FIELD_NAMES_TO_ATTRIBUTES = {attr: attr.lower().replace(" ", "_") for attr, *_ in INSPECTOR_FIELDS}
+FIELD_NAMES_TO_ATTRIBUTES = {
+    attr: attr.lower().replace(" ", "_") for attr, *_ in INSPECTOR_FIELDS
+}
