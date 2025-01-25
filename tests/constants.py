0c1dc35f1186295ed0de8d788e99546a0233dee4
Felipe Martins <felipe.martins4@hotmail.com>
2025-01-25 14:38:20 -0300

    refactor: add YouTube URL for testing

--- tests/constants.py
+++ tests/constants.py
@@ -3,5 +3,6 @@ from pathlib import Path
 EXAMPLE_MEDIA_PATH = str((Path(__file__).parent / "resources" / "example.mp3").resolve()).replace("\\", "/")
 EXAMPLE_MEDIA_DURATION = 9.952
 EXAMPLE_MEDIA_SCALE_FACTOR = EXAMPLE_MEDIA_DURATION / 100
+EXAMPLE_YT_URL = "https://youtu.be/6TtjniGQqAc?si=KDINcHVRsl4cH8Rn"
 
 EXAMPLE_OGG_DURATION = 9.952
