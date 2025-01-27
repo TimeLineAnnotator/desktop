8790336df7214f619aa62f91089e893b18436f5d
Felipe Martins <felipe.martins4@hotmail.com>
2025-01-27 19:40:13 -0300

    test: improve functions to interact with marker timeline

--- tests/ui/timelines/marker/interact.py
+++ tests/ui/timelines/marker/interact.py
@@ -1,14 +1,19 @@
-from tests.ui.timelines.interact import click_timeline_ui_view
+from tests.ui.timelines.interact import click_timeline_ui
 from tilia.ui.coords import time_x_converter
 
 
-def click_marker_ui(element, button="left", modifier=None, double=False):
-    click_timeline_ui_view(
-        element.timeline_ui.view,
-        button,
+def get_marker_ui_center(element):
+    return (
         time_x_converter.get_x_by_time(element.get_data("time")),
         element.timeline_ui.get_data("height") / 2,
-        element.body,
-        modifier,
-        double,
+    )
+
+
+def click_marker_ui(element, button="left", modifier=None, double=False):
+    click_timeline_ui(
+        element.timeline_ui,
+        element.get_data("time"),
+        button=button,
+        modifier=modifier,
+        double=double,
     )
