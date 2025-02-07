6f18432a4511d96773474a5abef1ba475172eaff
Felipe Martins <felipe.martins4@hotmail.com>
2025-02-07 10:52:49 -0300

    test: use fixtures instead of dummies

--- tests/timelines/hierarchy/test_hierarchy_component.py
+++ tests/timelines/hierarchy/test_hierarchy_component.py
@@ -1,73 +1,53 @@
-from tilia.timelines.hierarchy.components import Hierarchy
-from tilia.timelines.hierarchy.timeline import HierarchyTimeline
-
-
-class DummyHierarchyTL(HierarchyTimeline):
-    # noinspection PyMissingConstructor
-    def __init__(self):
-        pass
+from tilia.timelines.component_kinds import ComponentKind
 
 
 class TestHierarchyComponent:
-    def test_create(self):
-        params = {
-            "id": 0,
-            "start": 0,
-            "end": 1,
-            "level": 2,
-            "parent": "test_parent",
-            "children": ["test_children"],
-            "label": "test_label",
-            "comments": "test_comments",
-            "pre_start": "test_pre_start",
-            "post_end": "test_post_end",
-            "formal_type": "test_formal_type",
-            "formal_function": "test_formal_function",
-        }
-
-        hrc = Hierarchy(DummyHierarchyTL(), **params)
-
-        for attr, value in params.items():
-            assert getattr(hrc, attr) == params[attr]
-
-    def test_create_no_pre_start_or_end(self):
-        hrc = Hierarchy(DummyHierarchyTL(), 0, 0, 1, 1)
+    def test_create_no_pre_start_or_end(self, hierarchy_tl):
+        hrc, _ = hierarchy_tl.create_component(ComponentKind.HIERARCHY, 0, 1, 1)
 
         assert hrc.pre_start == 0
         assert hrc.post_end == 1
 
-    def test_set_start_changes_pre_start_previously_equal(self):
-        hrc = Hierarchy(DummyHierarchyTL(), 0, 1, 2, 1)
+    def test_set_start_changes_pre_start_previously_equal(self, hierarchy_tl):
+        hrc, _ = hierarchy_tl.create_component(ComponentKind.HIERARCHY, 1, 2, 1)
 
         hrc.start = 1.5
         assert hrc.pre_start == 1.5
 
-    def test_set_start_changes_pre_start_not_previously_equal_1(self):
-        hrc = Hierarchy(DummyHierarchyTL(), 0, 1, 2, 1, pre_start=0.5)
+    def test_set_start_changes_pre_start_not_previously_equal_1(self, hierarchy_tl):
+        hrc, _ = hierarchy_tl.create_component(
+            ComponentKind.HIERARCHY, 1, 2, 1, pre_start=0.5
+        )
 
         hrc.start = 0.8
         assert hrc.pre_start == 0.5
 
-    def test_set_start_changes_pre_start_not_previously_equal_2(self):
-        hrc = Hierarchy(DummyHierarchyTL(), 0, 1, 2, 1, pre_start=0.5)
+    def test_set_start_changes_pre_start_not_previously_equal_2(self, hierarchy_tl):
+        hrc, _ = hierarchy_tl.create_component(
+            ComponentKind.HIERARCHY, 1, 2, 1, pre_start=0.5
+        )
 
         hrc.start = 0.4
         assert hrc.pre_start == 0.4
 
-    def test_set_end_changes_post_end_previously_equal(self):
-        hrc = Hierarchy(DummyHierarchyTL(), 0, 1, 2, 1)
+    def test_set_end_changes_post_end_previously_equal(self, hierarchy_tl):
+        hrc, _ = hierarchy_tl.create_component(ComponentKind.HIERARCHY, 1, 2, 1)
 
         hrc.end = 1.5
         assert hrc.post_end == 1.5
 
-    def test_set_end_changes_post_end_not_previously_equal_1(self):
-        hrc = Hierarchy(DummyHierarchyTL(), 0, 1, 2, 1, post_end=2.5)
+    def test_set_end_changes_post_end_not_previously_equal_1(self, hierarchy_tl):
+        hrc, _ = hierarchy_tl.create_component(
+            ComponentKind.HIERARCHY, 1, 2, 1, post_end=2.5
+        )
 
         hrc.end = 2.3
         assert hrc.post_end == 2.5
 
-    def test_set_end_changes_post_end_not_previously_equal_2(self):
-        hrc = Hierarchy(DummyHierarchyTL(), 0, 1, 2, 1, post_end=2.5)
+    def test_set_end_changes_post_end_not_previously_equal_2(self, hierarchy_tl):
+        hrc, _ = hierarchy_tl.create_component(
+            ComponentKind.HIERARCHY, 1, 2, 1, post_end=2.5
+        )
 
         hrc.end = 2.8
         assert hrc.post_end == 2.8
