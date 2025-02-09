660604bd3187693c220cc97fd857ca1d879834db
Felipe Martins <felipe.martins4@hotmail.com>
2025-02-09 18:42:31 -0300

    refactor: make get_option a method
    This allows for less aggressive mocking.

--- tilia/ui/dialogs/by_time_or_by_measure.py
+++ tilia/ui/dialogs/by_time_or_by_measure.py
@@ -17,9 +17,9 @@ class ByTimeOrByMeasure(QDialog):
         layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         prompt = QLabel("Import using:")
-        time_radio = QRadioButton("Time")
+        self.time_radio = QRadioButton("Time")
         measure_and_fraction_radio = QRadioButton("Measure")
-        time_radio.setChecked(True)
+        self.time_radio.setChecked(True)
         button_box = QDialogButtonBox(
             QDialogButtonBox.StandardButton.Cancel | QDialogButtonBox.StandardButton.Ok
         )
@@ -27,11 +27,9 @@ class ByTimeOrByMeasure(QDialog):
         button_box.rejected.connect(self.reject)
 
         layout.addWidget(prompt)
-        layout.addWidget(time_radio)
+        layout.addWidget(self.time_radio)
         layout.addWidget(measure_and_fraction_radio)
         layout.addWidget(button_box)
 
-        def get_option():
-            return "time" if time_radio.isChecked() else "measure"
-
-        self.get_option = get_option
+    def get_option(self):
+        return "time" if self.time_radio.isChecked() else "measure"
