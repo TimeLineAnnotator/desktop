c43bea73654e154fb1ae416c8cd8cae2dbda8cba
Felipe Martins <felipe.martins4@hotmail.com>
2025-02-21 12:05:29 -0300

    refactor: mock at a lower level
    This makes the test cover more code and is also more resilient to refactoring, as `sentry_sdk.set_user` can be called in different ways in the future.

--- tests/ui/dialogs/test_crash.py
+++ tests/ui/dialogs/test_crash.py
@@ -15,27 +15,27 @@ class TestCrashDialog:
         crash_support_dialog.email_field.setText("a@valid.email")
         crash_support_dialog.name_field.setText("John Doe")
 
-        with patch("tilia.logging.logger.on_user_set") as set_user:
+        with patch("sentry_sdk.set_user") as set_user:
             crash_support_dialog.button_box.accepted.emit()
 
-        set_user.assert_called_with("a@valid.email", "John Doe")
+        set_user.assert_called_with({"name": "John Doe", "email": "a@valid.email"})
 
     def test_crash_support_invalid_email_sets_user(self):
         crash_support_dialog = CrashSupportDialog(None)
         crash_support_dialog.email_field.setText("an invalid email")
         crash_support_dialog.name_field.setText("John Doe")
 
-        with patch("tilia.logging.logger.on_user_set") as set_user:
+        with patch("sentry_sdk.set_user") as set_user:
             crash_support_dialog.button_box.accepted.emit()
 
-        set_user.assert_called_with("", "John Doe")
+        set_user.assert_called_with({"name": "John Doe", "email": ""})
 
     def test_crash_support_empty_fields(self):
         crash_support_dialog = CrashSupportDialog(None)
         crash_support_dialog.email_field.setText("")
         crash_support_dialog.name_field.setText("")
 
-        with patch("tilia.logging.logger.on_user_set") as set_user:
+        with patch("sentry_sdk.set_user") as set_user:
             crash_support_dialog.button_box.accepted.emit()
 
         set_user.assert_not_called()
