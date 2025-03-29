50fa7762c90eb290fc04421676f187ce8add8ef4
azfoo <45888544+azfoo@users.noreply.github.com>
2025-03-29 20:06:54 +0000

    refactor: move test into class

--- tests/test_export.py
+++ tests/test_export.py
@@ -11,136 +11,131 @@ from tilia.timelines.timeline_kinds import TimelineKind
 from tilia.ui.actions import TiliaAction
 
 
-def _trigger_export_action(user_actions, path):
-    with Serve(Get.FROM_USER_EXPORT_PATH, (True, path)):
-        user_actions.trigger(TiliaAction.FILE_EXPORT_JSON)
+class TestExportJSON:
+    def _trigger_export_action(self, user_actions, path):
+        with Serve(Get.FROM_USER_EXPORT_PATH, (True, path)):
+            user_actions.trigger(TiliaAction.FILE_EXPORT_JSON)
 
-    with open(path, encoding="utf-8") as f:
-        data = json.load(f)
+        with open(path, encoding="utf-8") as f:
+            data = json.load(f)
 
-    return data
+        return data
 
+    def test_timelines_attributes_are_exported(
+        self, tilia, marker_tlui, user_actions, tmp_path, use_test_settings
+    ):
+        # Marker timeline is chosen as an example. Ideally this should be parametrized for all timelines kinds.
+        tl_name = "my name"
+        with patch_ask_for_string_dialog(True, tl_name):
+            user_actions.trigger(TiliaAction.TIMELINE_NAME_SET)
 
-def test_timelines_attributes_are_exported(
-    tilia, marker_tlui, user_actions, tmp_path, use_test_settings
-):
-    # Marker timeline is chosen as an example. Ideally this should be parametrized for all timelines kinds.
-    tl_name = "my name"
-    with patch_ask_for_string_dialog(True, tl_name):
-        user_actions.trigger(TiliaAction.TIMELINE_NAME_SET)
+        tmp_file = tmp_path / "test.json"
 
-    tmp_file = tmp_path / "test.json"
+        data = self._trigger_export_action(user_actions, tmp_file)
 
-    data = _trigger_export_action(user_actions, tmp_file)
+        timeline_data = data["timelines"][0]
+        assert "hash" not in timeline_data
+        assert timeline_data["ordinal"] == 1
+        assert timeline_data["name"] == tl_name
+        assert timeline_data["is_visible"] is True
+        assert timeline_data["height"] == settings.get(
+            "marker_timeline", "default_height"
+        )
+        assert timeline_data["kind"] == TimelineKind.MARKER_TIMELINE.name
+        assert timeline_data["components"] == []
 
-    timeline_data = data["timelines"][0]
-    assert "hash" not in timeline_data
-    assert timeline_data["ordinal"] == 1
-    assert timeline_data["name"] == tl_name
-    assert timeline_data["is_visible"] is True
-    assert timeline_data["height"] == settings.get("marker_timeline", "default_height")
-    assert timeline_data["kind"] == TimelineKind.MARKER_TIMELINE.name
-    assert timeline_data["components"] == []
+    def test_timeline_not_exportable_attributes_are_not_exported(
+        self, tilia, harmony_tlui, user_actions, tmp_path
+    ):
+        # Harmony timeline is chosen as an example. Ideally this should be parametrized for all timelines kinds that have not exportable attributes.
+        tmp_file = tmp_path / "test.json"
+        data = self._trigger_export_action(user_actions, tmp_file)
 
+        timeline_data = data["timelines"][0]
+        for attr in HarmonyTimeline.NOT_EXPORTABLE_ATTRS:
+            assert attr not in timeline_data
 
-def test_timeline_not_exportable_attributes_are_not_exported(
-    tilia, harmony_tlui, user_actions, tmp_path
-):
-    # Harmony timeline is chosen as an example. Ideally this should be parametrized for all timelines kinds that have not exportable attributes.
-    tmp_file = tmp_path / "test.json"
-    data = _trigger_export_action(user_actions, tmp_file)
+    def test_not_exportble_timelines_are_not_exported(
+        self, tilia, marker_tlui, audiowave_tlui, user_actions, tmp_path
+    ):
+        # Audiowave timeline is chosen as an example. Ideally this should be parametrized for all non-exportable timeline types.
+        tmp_file = tmp_path / "test.json"
+        data = self._trigger_export_action(user_actions, tmp_file)
 
-    timeline_data = data["timelines"][0]
-    for attr in HarmonyTimeline.NOT_EXPORTABLE_ATTRS:
-        assert attr not in timeline_data
+        assert len(data["timelines"]) == 1
 
+    def test_export_timelines(
+        self, tilia, marker_tlui, harmony_tlui, user_actions, tilia_state, tmp_path
+    ):
+        for i in range(5):
+            tilia_state.current_time = i
+            user_actions.trigger(TiliaAction.MARKER_ADD)
 
-def test_not_exportble_timelines_are_not_exported(
-    tilia, marker_tlui, audiowave_tlui, user_actions, tmp_path
-):
-    # Audiowave timeline is chosen as an example. Ideally this should be parametrized for all non-exportable timeline types.
-    tmp_file = tmp_path / "test.json"
-    data = _trigger_export_action(user_actions, tmp_file)
+        harmony_tlui.create_harmony(0)
+        harmony_tlui.create_mode(0)
 
-    assert len(data["timelines"]) == 1
+        tmp_file = tmp_path / "test.json"
 
+        data = self._trigger_export_action(user_actions, tmp_file)
 
-def test_export_timelines(
-    tilia, marker_tlui, harmony_tlui, user_actions, tilia_state, tmp_path
-):
-    for i in range(5):
-        tilia_state.current_time = i
-        user_actions.trigger(TiliaAction.MARKER_ADD)
+        assert len(data["timelines"]) == 2
+        assert data["timelines"][0]["kind"] == "MARKER_TIMELINE"
+        assert data["timelines"][1]["kind"] == "HARMONY_TIMELINE"
 
-    harmony_tlui.create_harmony(0)
-    harmony_tlui.create_mode(0)
+        # check marker timeline components
+        components = data["timelines"][0]["components"]
+        assert len(components) == 5
+        assert [c["kind"] for c in components] == ["MARKER"] * 5
+        assert [c["time"] for c in components] == [0, 1, 2, 3, 4]
 
-    tmp_file = tmp_path / "test.json"
+        # check harmony timeline components
+        components = data["timelines"][1]["components"]
+        assert len(components) == 2
+        assert [c["kind"] for c in components] == ["HARMONY", "MODE"]
+        assert [c["time"] for c in components] == [0, 0]
 
-    data = _trigger_export_action(user_actions, tmp_file)
+    def test_export_has_media_path(self, tilia, user_actions, tmp_path):
+        post(Post.APP_MEDIA_LOAD, EXAMPLE_MEDIA_PATH)
 
-    assert len(data["timelines"]) == 2
-    assert data["timelines"][0]["kind"] == "MARKER_TIMELINE"
-    assert data["timelines"][1]["kind"] == "HARMONY_TIMELINE"
+        tmp_file = tmp_path / "test.json"
 
-    # check marker timeline components
-    components = data["timelines"][0]["components"]
-    assert len(components) == 5
-    assert [c["kind"] for c in components] == ["MARKER"] * 5
-    assert [c["time"] for c in components] == [0, 1, 2, 3, 4]
+        data = self._trigger_export_action(user_actions, tmp_file)
 
-    # check harmony timeline components
-    components = data["timelines"][1]["components"]
-    assert len(components) == 2
-    assert [c["kind"] for c in components] == ["HARMONY", "MODE"]
-    assert [c["time"] for c in components] == [0, 0]
+        assert data["media_path"] == EXAMPLE_MEDIA_PATH
 
+    def test_export_has_media_metadata(self, tilia, user_actions, tmp_path):
+        post(Post.MEDIA_METADATA_FIELD_SET, "title", "Test Title")
 
-def test_export_has_media_path(tilia, user_actions, tmp_path):
-    post(Post.APP_MEDIA_LOAD, EXAMPLE_MEDIA_PATH)
+        tmp_file = tmp_path / "test.json"
 
-    tmp_file = tmp_path / "test.json"
+        data = self._trigger_export_action(user_actions, tmp_file)
 
-    data = _trigger_export_action(user_actions, tmp_file)
+        assert data["media_metadata"]["title"] == "Test Title"
 
-    assert data["media_path"] == EXAMPLE_MEDIA_PATH
+    def test_export_without_path(self, tilia, user_actions, tmp_path):
+        tmp_file = tmp_path / "test.json"
 
+        with Serve(Get.FROM_USER_EXPORT_PATH, (True, tmp_file)):
+            post(Post.FILE_EXPORT)
 
-def test_export_has_media_metadata(tilia, user_actions, tmp_path):
-    post(Post.MEDIA_METADATA_FIELD_SET, "title", "Test Title")
+        assert tmp_file.exists()
 
-    tmp_file = tmp_path / "test.json"
+    @parametrize_component
+    def test_exported_component_attributes_values_are_correct(
+        self, tilia, user_actions, comp, tmp_path, request
+    ):
+        comp = request.getfixturevalue(comp)
+        if TimelineFlag.NOT_EXPORTABLE in comp.timeline.FLAGS:
+            return
 
-    data = _trigger_export_action(user_actions, tmp_file)
+        tmp_file = tmp_path / "test.json"
 
-    assert data["media_metadata"]["title"] == "Test Title"
+        data = self._trigger_export_action(user_actions, tmp_file)
 
+        exported_component = data["timelines"][0]["components"][0]
 
-def test_export_without_path(tilia, user_actions, tmp_path):
-    tmp_file = tmp_path / "test.json"
-
-    with Serve(Get.FROM_USER_EXPORT_PATH, (True, tmp_file)):
-        post(Post.FILE_EXPORT)
-
-    assert tmp_file.exists()
-
-
-@parametrize_component
-def test_exported_component_attributes_values_are_correct(
-    tilia, user_actions, comp, tmp_path, request
-):
-    comp = request.getfixturevalue(comp)
-    if TimelineFlag.NOT_EXPORTABLE in comp.timeline.FLAGS:
-        return
-
-    tmp_file = tmp_path / "test.json"
-
-    data = _trigger_export_action(user_actions, tmp_file)
-
-    exported_component = data["timelines"][0]["components"][0]
-
-    for attr in comp.get_export_attributes():
-        comp_value = getattr(comp, attr)
-        if isinstance(comp_value, tuple):
-            comp_value = list(comp_value)  # JSON converts tuples to lists
-        assert comp_value == exported_component[attr]
+        for attr in comp.get_export_attributes():
+            comp_value = getattr(comp, attr)
+            if isinstance(comp_value, tuple):
+                comp_value = list(comp_value)  # JSON converts tuples to lists
+            assert comp_value == exported_component[attr]
