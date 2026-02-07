793d67fe683e44ac79062e3f57589772765d3d04
Felipe Martins <felipe.martins4@hotmail.com>
2026-02-07 12:50:57 +0100

    refactor: add cli.io.error function

--- tilia/ui/cli/timelines/utils.py
+++ tilia/ui/cli/timelines/utils.py
@@ -1,8 +1,6 @@
 from contextlib import contextmanager
 from unittest.mock import patch
 
-from colorama import Fore
-
 from tilia.requests import get, Get
 from tilia.ui.cli import io
 
@@ -11,7 +9,7 @@ def get_timeline_by_name(name: str):
     tls = get(Get.TIMELINES_BY_ATTR, "name", name)
 
     if not tls:
-        io.output(f"No timeline found with name={name}", Fore.RED)
+        io.error(f"No timeline found with name={name}")
         return False, None
     if len(tls) == 1:
         return True, tls[0]
@@ -24,7 +22,7 @@ def get_timeline_by_ordinal(ordinal: int):
     tl = get(Get.TIMELINE_BY_ATTR, "ordinal", ordinal)
 
     if not tl:
-        io.output(f"No timeline found with ordinal={ordinal}", Fore.RED)
+        io.error(f"No timeline found with ordinal={ordinal}")
         return False, None
     else:
         return True, tl
@@ -34,3 +32,25 @@ def get_timeline_by_ordinal(ordinal: int):
 def patch_warn():
     with patch("tilia.ui.cli.io.warn") as mock_warn:
         yield mock_warn
+
+
+@contextmanager
+def assert_warn():
+    with patch("tilia.ui.cli.io.warn") as mock_warn:
+        yield mock_warn
+
+    mock_warn.assert_called()
+
+
+@contextmanager
+def patch_error():
+    with patch("tilia.ui.cli.io.error") as mock_warn:
+        yield mock_warn
+
+
+@contextmanager
+def assert_error():
+    with patch("tilia.ui.cli.io.error") as mock_error:
+        yield mock_error
+
+    mock_error.assert_called()
