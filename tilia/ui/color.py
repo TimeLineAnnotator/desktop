cb5fbd3ddc6be5e040e2d908e854130866d8b326
Felipe Martins <felipe.martins4@hotmail.com>
2026-02-21 19:04:15 +0100

    fix: typing

--- tilia/ui/color.py
+++ tilia/ui/color.py
@@ -1,9 +1,9 @@
 from PySide6.QtGui import QColor
 
 
-def get_tinted_color(color: str, factor: int) -> str:
+def get_tinted_color(color: str | QColor, factor: int) -> str:
     return QColor(color).darker(factor).name()
 
 
-def get_untinted_color(color: str, factor: int) -> str:
+def get_untinted_color(color: str | QColor, factor: int) -> str:
     return QColor(color).lighter(factor).name()
