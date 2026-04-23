251862ad9e276330f853fba66b07e12d1915a8fc
azfoo <45888544+azfoo@users.noreply.github.com>
2026-04-23 21:12:51 +0100

    test: yes/no cli

--- tests/ui/cli/test_io.py
+++ tests/ui/cli/test_io.py
@@ -7,21 +7,22 @@ from tilia.ui.cli.io import ask_yes_or_no
 
 class TestInput:
     @pytest.mark.parametrize(
-        "user_input,expected",
+        "user_input,expected,count",
         [
-            ("y", True),
-            ("Y", True),
-            ("yes", True),
-            ("YES", True),
-            ("Yes", True),
-            ("n", False),
-            ("N", False),
-            ("no", False),
-            ("NO", False),
-            ("anything", False),
-            ("", False),
+            ("y", True, 1),
+            ("Y", True, 1),
+            ("yes", True, 1),
+            ("YES", True, 1),
+            ("Yes", True, 1),
+            ("n", False, 1),
+            ("N", False, 1),
+            ("no", False, 1),
+            ("NO", False, 1),
+            (["anything", "no"], False, 2),
+            ([""], True, 1),
         ],
     )
-    def test_ask_yes_or_no(self, user_input, expected):
-        with patch("builtins.input", return_value=user_input):
+    def test_ask_yes_or_no(self, user_input, expected, count):
+        with patch("builtins.input", side_effect=user_input) as q:
             assert ask_yes_or_no("Some prompt") == expected
+        assert q.call_count == count
