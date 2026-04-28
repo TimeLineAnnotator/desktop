048aaa234c1576e742b5a52e8a999e49a6ec059b
Felipe Martins <felipe.martins4@hotmail.com>
2026-04-28 11:54:11 +0200

    feat: improve error message

--- tilia/errors.py
+++ tilia/errors.py
@@ -47,7 +47,7 @@ YOUTUBE_URL_INVALID = Error("Invalid YouTube URL", "{} is not a valid URL.")
 EXPORT_AUDIO_FAILED = Error("Export Audio", "{}")
 INVALID_HARMONY_INVERSION = Error(
     "Invalid harmony inversion",
-    "Can't set inversion '{}' on a letter of type '{}'. Please select a valid inversion for this letter type.",
+    "Can't set inversion '{}' on a chord of type '{}'. Please select a valid inversion for this chord type.",
 )
 ADD_MODE_FAILED = Error("Add key failed", "Adding key failed: {}.")
 ADD_HARMONY_FAILED = Error("Add harmony failed", "{}")
