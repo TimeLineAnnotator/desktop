069c37c242a2c85eaad1ee8299544698941b1a83
azfoo <45888544+azfoo@users.noreply.github.com>
2026-05-17 01:58:01 +0100

    fix: use QGraphicsItem.setCursor instead of override

--- tilia/ui/timelines/cursors.py
+++ tilia/ui/timelines/cursors.py
@@ -1,20 +1,6 @@
-from PySide6.QtGui import QGuiApplication
-
-
 # noinspection PyUnresolvedReferences
 class CursorMixIn:
     def __init__(self, cursor_shape, *args, **kwargs):
         super().__init__(*args, *kwargs)
-        self.cursor_shape = cursor_shape
+        self.setCursor(cursor_shape)
         self.setAcceptHoverEvents(True)
-
-    def hoverEnterEvent(self, event) -> None:
-        QGuiApplication.setOverrideCursor(self.cursor_shape)
-        super().hoverEnterEvent(event)
-
-    def hoverLeaveEvent(self, event) -> None:
-        QGuiApplication.restoreOverrideCursor()
-        super().hoverLeaveEvent(event)
-
-    def cleanup(self):
-        QGuiApplication.restoreOverrideCursor()
